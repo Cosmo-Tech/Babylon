ddb2ffe3919e61c8001e33b2a388c3f9e3604870
afossart <alexis.fossart@cosmotech.com>
2022-09-02 14:57:31 +0200

    renamed index template to `index.template.md`
    This change allows to keep the integration of the markdown in IDEs

--- scripts/generate_index.py
+++ scripts/generate_index.py
@@ -5,7 +5,7 @@ import mkdocs_gen_files
 from Babylon.version import VERSION
 
 _md_file: IO
-with mkdocs_gen_files.open("index.md", "w") as _md_file, open("scripts/index.md.template") as index_template:
+with mkdocs_gen_files.open("index.md", "w") as _md_file, open("scripts/index.template.md") as index_template:
     _index: list[str] = index_template.readlines()
     for _line in _index:
         _md_file.write(_line.replace("VERSION", VERSION))
