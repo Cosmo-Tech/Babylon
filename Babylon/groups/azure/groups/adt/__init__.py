cb9490a2565864d8b65feb551ce9374d16d1c308
afossart <alexis.fossart@cosmotech.com>
2022-09-12 16:31:21 +0200

    Added command `azure adt upload-model`
    This command allows to upload a new model to ADT, with an override option to replace existing models
    (ADT does not have an update)

---
+++ Babylon/groups/azure/groups/adt/__init__.py
@@ -0,0 +1,25 @@
+from azure.digitaltwins.core import DigitalTwinsClient
+from click import group
+from click import pass_context
+from click.core import Context
+
+from .commands import list_commands
+from .groups import list_groups
+
+from .....utils.decorators import require_deployment_key
+
+@group()
+@pass_context
+@require_deployment_key("digital_twin_url", "digital_twin_url")
+def adt(ctx: Context, digital_twin_url: str):
+    """Allow communication with Azure Digital Twin"""
+
+    ctx.obj = DigitalTwinsClient(credential=ctx.parent.obj,
+                                 endpoint=digital_twin_url)
+
+
+for _command in list_commands:
+    adt.add_command(_command)
+
+for _group in list_groups:
+    adt.add_command(_group)
---
+++ Babylon/groups/azure/groups/adt/commands/__init__.py
@@ -0,0 +1,5 @@
+from .upload_model import upload_model
+
+list_commands = [
+    upload_model,
+]
