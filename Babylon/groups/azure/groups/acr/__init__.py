253c70a43b539795e724b19a2ed453f54cacc7cc
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2022-10-06 15:02:28 +0200

    fixed az system call should require az program

--- Babylon/groups/azure/groups/acr/__init__.py
+++ Babylon/groups/azure/groups/acr/__init__.py
@@ -3,13 +3,14 @@ from click import group
 from click import pass_context
 from click.core import Context
 from azure.containerregistry import ContainerRegistryClient
-from Babylon.utils.decorators import require_deployment_key
+from Babylon.utils.decorators import require_deployment_key, requires_external_program
 from .commands import list_commands
 from .groups import list_groups
 
 
 @group()
 @pass_context
+@requires_external_program("az")
 @require_deployment_key("acr_registry_name", "acr_registry_name")
 def acr(ctx: Context, acr_registry_name: str):
     """Group initialized from a template"""
