a8dded960b90a1da67d1cf1dfaa574399992cfa9
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2022-10-10 11:40:48 +0200

    Fixed azure acr ls command

--- Babylon/groups/azure/groups/acr/commands/ls.py
+++ Babylon/groups/azure/groups/acr/commands/ls.py
@@ -1,5 +1,6 @@
+import os
 import logging
-from click import command, make_pass_decorator
+from click import command, make_pass_decorator, pass_context, Context
 from azure.containerregistry import ContainerRegistryClient
 from Babylon.utils.decorators import require_deployment_key
 
@@ -11,13 +12,17 @@ Should list all images in the given registry
 Can be checked against `az acr repository list --name my_registry --output table`
 """
 
-pass_cr_client = make_pass_decorator(ContainerRegistryClient)
-
 @command()
-@pass_cr_client
-@require_deployment_key("acr_registry_name", "acr_registry_name")
-def ls(cr_client: ContainerRegistryClient, acr_registry_name: str):
+@pass_context
+@require_deployment_key("acr_src_registry_name", "acr_src_registry_name")
+def ls(ctx: Context, acr_src_registry_name: str):
     """List all docker images in the specified registry"""
-    logger.info("Getting repositories stored in registry %s", acr_registry_name)
+    # Login to registry
+    os.system(f"az acr login --name {acr_src_registry_name}")
+    cr_client = ContainerRegistryClient(
+        f"https://{acr_src_registry_name}",
+        ctx.parent.obj,
+        audience="https://management.azure.com")
+    logger.info("Getting repositories stored in registry %s", acr_src_registry_name)
     for repo in cr_client.list_repository_names():
         logger.info(f"repository: {repo}")
