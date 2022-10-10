b4c707df8b42e4afadfd4f9354ccb4558c0176d3
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2022-10-10 11:36:40 +0200

    Feedbacks - PR #21 - Removed AzureCliCredentials check - Fixed acr docstring - Use fstring in logs

--- Babylon/groups/azure/groups/acr/__init__.py
+++ Babylon/groups/azure/groups/acr/__init__.py
@@ -1,14 +1,16 @@
 from click import group
 from click import pass_context
 from click.core import Context
+from Babylon.utils.decorators import requires_external_program
 from .commands import list_commands
 from .groups import list_groups
 
 
 @group()
+@requires_external_program("docker")
 @pass_context
 def acr(ctx: Context):
-    """Nothing needed here"""
+    """Group interacting with Azure Container Registry"""
 
 for _command in list_commands:
     acr.add_command(_command)
--- Babylon/groups/azure/groups/acr/commands/ls.py
+++ Babylon/groups/azure/groups/acr/commands/ls.py
@@ -1,7 +1,7 @@
 import logging
 from click import command, make_pass_decorator
 from azure.containerregistry import ContainerRegistryClient
-from Babylon.utils.decorators import requires_external_program, require_deployment_key
+from Babylon.utils.decorators import require_deployment_key
 
 logger = logging.getLogger("Babylon")
 
@@ -14,11 +14,10 @@ Can be checked against `az acr repository list --name my_registry --output table
 pass_cr_client = make_pass_decorator(ContainerRegistryClient)
 
 @command()
-@requires_external_program("docker")
 @pass_cr_client
 @require_deployment_key("acr_registry_name", "acr_registry_name")
 def ls(cr_client: ContainerRegistryClient, acr_registry_name: str):
     """List all docker images in the specified registry"""
     logger.info("Getting repositories stored in registry %s", acr_registry_name)
     for repo in cr_client.list_repository_names():
-        logger.info("repository: %s", repo)
+        logger.info(f"repository: {repo}")
