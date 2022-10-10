be634ae5fe86ba82df2feb501057b0c58466489e
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2022-10-10 14:11:01 +0200

    Changed pull/push to accept optional arguments

--- Babylon/groups/azure/groups/acr/commands/ls.py
+++ Babylon/groups/azure/groups/acr/commands/ls.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from click import command, make_pass_decorator, pass_context, Context
+from click import command, pass_context, Context, option
 from azure.containerregistry import ContainerRegistryClient
 from Babylon.utils.decorators import require_deployment_key
 
@@ -8,21 +8,29 @@ logger = logging.getLogger("Babylon")
 
 """Command Tests
 > babylon acr ls
+Should list all images in the registry given in config
+> babylon acr ls -r my_registry
 Should list all images in the given registry
 Can be checked against `az acr repository list --name my_registry --output table`
 """
 
+
 @command()
 @pass_context
 @require_deployment_key("acr_src_registry_name", "acr_src_registry_name")
-def ls(ctx: Context, acr_src_registry_name: str):
+@option("-r", "--registry")
+def ls(ctx: Context, acr_src_registry_name: str, registry: str):
     """List all docker images in the specified registry"""
+    registry = registry or acr_src_registry_name
     # Login to registry
-    os.system(f"az acr login --name {acr_src_registry_name}")
+    response = os.system(f"az acr login --name {registry}")
+    if response:
+        logger.error(f"Could not connect to registry {registry}")
+        return
     cr_client = ContainerRegistryClient(
-        f"https://{acr_src_registry_name}",
+        f"https://{registry}",
         ctx.parent.obj,
         audience="https://management.azure.com")
-    logger.info("Getting repositories stored in registry %s", acr_src_registry_name)
+    logger.info("Getting repositories stored in registry %s", registry)
     for repo in cr_client.list_repository_names():
         logger.info(f"repository: {repo}")
