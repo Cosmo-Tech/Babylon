b29003bdebc745465041ec3a79f80602cba366e7
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2022-10-11 11:17:59 +0200

    Testing string logging and working dir - Simplified logging multilinehandler - Changed logic flow of to_header_line

--- Babylon/utils/string.py
+++ Babylon/utils/string.py
@@ -16,15 +16,14 @@ def is_valid_command_name(string: str) -> bool:
 def to_header_line(string: str) -> str:
     """
     Convert a string to a header line of maximum length of MAX_LINE_LENGTH
-    
+
     will center the string between `-` to show a delimitation 
     :param string: the string to transform
     :return: the string centered with `-` padding
     """
     if (length := len(string)) > MAX_LINE_LENGTH:
         return string
-    elif length == 0:
+    if length == 0:
         return "-" * MAX_LINE_LENGTH
-    else:
-        missing = MAX_LINE_LENGTH - length - 2
-        return f"{'-' * (missing // 2)} {string} {'-' * (missing // 2)}"
+    missing = MAX_LINE_LENGTH - length - 2
+    return f"{'-' * (missing // 2)} {string} {'-' * (missing // 2)}"
