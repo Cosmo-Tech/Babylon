653eece522d123b0577fb5f1aa7b94fd4a6962c5
Alexis Fossart <alexis.fossart@cosmotech.com>
2022-10-12 10:24:29 +0200

    Cleanup of the code in group `api connector`
    The cleanup ensure a closer relation to the format of existing files

--- Babylon/groups/api/groups/connector/commands/create.py
+++ Babylon/groups/api/groups/connector/commands/create.py
@@ -2,18 +2,25 @@ from logging import getLogger
 from pprint import pformat
 from typing import Optional
 
-from click import argument, command, make_pass_decorator, option, Choice
+from click import Choice
+from click import argument
+from click import command
+from click import make_pass_decorator
+from click import option
 from cosmotech_api.api.connector_api import ConnectorApi
 from cosmotech_api.exceptions import UnauthorizedException
 
-from Babylon.utils.api import get_api_file
 from Babylon.utils import TEMPLATE_FOLDER_PATH
-from Babylon.utils.decorators import allow_dry_run, pass_environment, timing_decorator
+from Babylon.utils.api import get_api_file
+from Babylon.utils.decorators import allow_dry_run
+from Babylon.utils.decorators import pass_environment
+from Babylon.utils.decorators import timing_decorator
 from Babylon.utils.environment import Environment
 
 logger = getLogger("Babylon")
 
 pass_connector_api = make_pass_decorator(ConnectorApi)
+
 
 @command()
 @allow_dry_run
@@ -21,47 +28,37 @@ pass_connector_api = make_pass_decorator(ConnectorApi)
 @pass_connector_api
 @pass_environment
 @argument("connector_file", type=str, required=False)
-@option(
-    "-e",
-    "--use-working-dir-file",
-    "use_working_dir_file",
-    is_flag=True,
-    help="Should the path be relative to the working directory ?",
-)
-@option(
-    "-t",
-    "--type",
-    "connector_type",
-    required=True,
-    type=Choice(["ADT", "STORAGE"], case_sensitive=False),
-    help="Connector type, allowed values : [ADT, STORAGE]",
-)
-@option(
-    "-n",
-    "--name",
-    "connector_name",
-    required=True,
-    type=str,
-    help="New connector name",
-)
-@option(
-    "-v",
-    "--version",
-    "connector_version",
-    required=True,
-    type=str,
-    help="New connector version",
-)
-def create(
-    env: Environment,
-    connector_api: ConnectorApi,
-    connector_type: str,
-    connector_name: str,
-    connector_version: str,
-    connector_file: Optional[str] = None,
-    use_working_dir_file: bool = False,
-    dry_run: bool = False,
-):
+@option("-e",
+        "--use-working-dir-file",
+        "use_working_dir_file",
+        is_flag=True,
+        help="Should the path be relative to the working directory ?")
+@option("-t",
+        "--type",
+        "connector_type",
+        required=True,
+        type=Choice(["ADT", "STORAGE"], case_sensitive=False),
+        help="Connector type, allowed values : [ADT, STORAGE]")
+@option("-n",
+        "--name",
+        "connector_name",
+        required=True,
+        type=str,
+        help="New connector name")
+@option("-v",
+        "--version",
+        "connector_version",
+        required=True,
+        type=str,
+        help="New connector version")
+def create(env: Environment,
+           connector_api: ConnectorApi,
+           connector_type: str,
+           connector_name: str,
+           connector_version: str,
+           connector_file: Optional[str] = None,
+           use_working_dir_file: bool = False,
+           dry_run: bool = False):
     """Register new connector by sending a JSON or YAML file to the API."""
 
     connector_type = connector_type.upper()
@@ -86,13 +83,10 @@ def create(
     converted_connector_content["key"] = connector_name.replace(" ", "")
     converted_connector_content["version"] = connector_version
     try:
-        retrieved_connector = connector_api.register_connector(
-            connector=converted_connector_content
-        )
+        retrieved_connector = connector_api.register_connector(connector=converted_connector_content)
     except UnauthorizedException:
         logger.error("Unauthorized access to the cosmotech api")
-    env.configuration.set_deploy_var(
-        f"{connector_type.lower()}_connector_id", retrieved_connector["id"]
-    )
+        return
+    env.configuration.set_deploy_var(f"{connector_type.lower()}_connector_id", retrieved_connector["id"])
     logger.debug(pformat(retrieved_connector))
     logger.info("Created new %s Connector with id: %s", connector_type, retrieved_connector['id'])
