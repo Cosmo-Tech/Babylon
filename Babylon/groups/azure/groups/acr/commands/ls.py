76c5b9e54c28d94d160b5a690b846500df869cb1
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2022-10-12 17:02:38 +0200

    Changed coding style - separated imports - added help on options - reformated code with yapf - Using .. instead of Babylon on imports

--- Babylon/groups/azure/groups/acr/commands/ls.py
+++ Babylon/groups/azure/groups/acr/commands/ls.py
@@ -1,15 +1,20 @@
-import subprocess
 import logging
-from click import command, pass_context, Context, option
+import subprocess
+import typing
+
 from azure.containerregistry import ContainerRegistryClient
-from Babylon.utils.decorators import require_deployment_key
+from click import command
+from click import Context
+from click import option
+from click import pass_context
+
+from ......utils.decorators import require_deployment_key
 
 logger = logging.getLogger("Babylon")
-
 """Command Tests
-> babylon acr ls
+> babylon azure acr ls
 Should list all images in the registry given in config
-> babylon acr ls -r my_registry
+> babylon azure acr ls -r my_registry
 Should list all images in the given registry
 Can be checked against `az acr repository list --name my_registry --output table`
 """
@@ -18,21 +23,20 @@ Can be checked against `az acr repository list --name my_registry --output table
 @command()
 @pass_context
 @require_deployment_key("acr_src_registry_name", "acr_src_registry_name")
-@option("-r", "--registry")
-def ls(ctx: Context, acr_src_registry_name: str, registry: str):
+@option("-r", "--registry", help="Container Registry name to scan, example: myregistry.azurecr.io")
+def ls(ctx: Context, acr_src_registry_name: str, registry: typing.Optional[str]):
     """List all docker images in the specified registry"""
     registry = registry or acr_src_registry_name
     # Login to registry
     response = subprocess.run(["az", "acr", "login", "--name", registry],
-                              shell=False, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                              shell=False,
+                              check=True,
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE)
     if response.returncode:
-        logger.error(
-            f"Could not connect to registry {registry}: {response.stderr}")
+        logger.error(f"Could not connect to registry {registry}: {response.stderr}")
         return
-    cr_client = ContainerRegistryClient(
-        f"https://{registry}",
-        ctx.parent.obj,
-        audience="https://management.azure.com")
+    cr_client = ContainerRegistryClient(f"https://{registry}", ctx.parent.obj, audience="https://management.azure.com")
     logger.info("Getting repositories stored in registry %s", registry)
     for repo in cr_client.list_repository_names():
         logger.info(f"repository: {repo}")
--- Babylon/groups/azure/groups/acr/commands/pull.py
+++ Babylon/groups/azure/groups/acr/commands/pull.py
@@ -1,13 +1,18 @@
-import subprocess
 import logging
-import docker
-from click import command, pass_context, option
-from click.core import Context
+import subprocess
+import typing
+
 from azure.containerregistry import ContainerRegistryClient
-from Babylon.utils.decorators import require_deployment_key
+from click import command
+from click import Context
+from click import option
+from click import pass_context
+import docker
+from docker import errors as dock_errors
+
+from ......utils.decorators import require_deployment_key
 
 logger = logging.getLogger("Babylon")
-
 """Command Tests
 > babylon azure acr pull
 Should add a new entry to `docker image ls`
@@ -22,31 +27,32 @@ Should add new entry to `docker image ls`
 @pass_context
 @require_deployment_key("acr_src_registry_name", "acr_src_registry_name")
 @require_deployment_key("acr_image_reference", "acr_image_reference")
-@option("-i", "--image")
-@option("-r", "--registry")
-def pull(ctx: Context, acr_src_registry_name: str, acr_image_reference: str, registry: str, image: str):
+@option("-i", "--image", help="Local docker image to pull")
+@option("-r", "--registry", help="Container Registry name to pull from, example: myregistry.azurecr.io")
+def pull(ctx: Context, acr_src_registry_name: str, acr_image_reference: str, registry: typing.Optional[str],
+         image: typing.Optional[str]):
     """Pulls a docker image from the ACR registry given in deployment configuration"""
     registry = registry or acr_src_registry_name
     image = image or acr_image_reference
     # Login to registry
-    response = subprocess.run(["az", "acr", "login", "--name", registry],
-                              shell=False, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    response = subprocess.run(
+        ["az", "acr", "login", "--name", registry],
+        shell=False,
+        check=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
     if response.returncode:
-        logger.error(
-            f"Could not connect to registry {registry}: {response.stderr}")
+        logger.error(f"Could not connect to registry {registry}: {str(response.stderr)}")
         return
-    ContainerRegistryClient(
-        f"https://{registry}",
-        ctx.parent.obj,
-        audience="https://management.azure.com")
+    ContainerRegistryClient(f"https://{registry}", ctx.parent.obj, audience="https://management.azure.com")
     # Pulling image
     client = docker.from_env()
     logger.info(f"Pulling image {image}")
     repo = f"{registry}/{image}"
     try:
         client.images.pull(repository=repo)
-    except docker.errors.NotFound:
-        logger.error(
-            f"Registry {registry} does not contain {image}")
-    except docker.errors.APIError as api_error:
+    except dock_errors.NotFound:
+        logger.error(f"Registry {registry} does not contain {image}")
+    except dock_errors.APIError as api_error:
         logger.error(f"API Error: {api_error}")
