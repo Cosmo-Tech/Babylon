cf2b2af86433216322c2a510b2e277062fe55395
johnfolly <john.folly@cosmotech.com>
2022-10-13 17:20:25 +0200

    added api solution subgroup commands : create and update to improve

--- Babylon/groups/api/groups/solution/commands/__init__.py
+++ Babylon/groups/api/groups/solution/commands/__init__.py
@@ -1,11 +1,15 @@
+from .delete import delete
+from .get_current import get_current
+from .get_all import get_all
 from .create import create
 from .get import get
-from .get_all_ids import get_all_ids
 from .update import update
 
 list_commands = [
+    delete,
+    get_current,
+    get_all,
     create,
     get,
-    get_all_ids,
     update,
 ]
--- Babylon/groups/api/groups/solution/commands/create.py
+++ Babylon/groups/api/groups/solution/commands/create.py
@@ -1,49 +1,82 @@
-import logging
+from logging import getLogger
 from pprint import pformat
 
-import cosmotech_api
 from click import argument
 from click import command
 from click import make_pass_decorator
 from click import option
 from cosmotech_api.api.solution_api import SolutionApi
+from cosmotech_api.exceptions import NotFoundException
+from cosmotech_api.exceptions import UnauthorizedException
 
 from ......utils.api import get_api_file
 from ......utils.decorators import allow_dry_run
+from ......utils.decorators import pass_environment
 from ......utils.decorators import require_deployment_key
 from ......utils.decorators import timing_decorator
+from ......utils.environment import Environment
 
-logger = logging.getLogger("Babylon")
+logger = getLogger("Babylon")
 
 pass_solution_api = make_pass_decorator(SolutionApi)
 
 
 @command()
-@pass_solution_api
-@require_deployment_key("organization_id", "organization_id")
-@argument("solution_file")
-@option("-e", "--use-solution-file", "use_solution_file", is_flag=True,
-        help="Should the path be in the solution ?")
 @allow_dry_run
 @timing_decorator
-def create(solution_api: SolutionApi,
-           organization_id: str,
-           solution_file: str,
-           use_solution_file: bool = False,
-           dry_run: bool = False):
-    """Send a JSON or YAML file to the API to create a solution"""
+@pass_solution_api
+@pass_environment
+@argument("solution_file", type=str)
+@require_deployment_key("organization_id", "organization_id")
+@option(
+    "-s",
+    "--select",
+    "select",
+    type=bool,
+    help="Should ...",
+    required=False,
+)
+@option(
+    "-e",
+    "--use-working-dir-file",
+    "use_working_dir_file",
+    is_flag=True,
+    help="Should the path be relative to the working directory ?",
+)
+def create(
+    env: Environment,
+    solution_api: SolutionApi,
+    organization_id: str,
+    solution_file: str,
+    use_working_dir_file: bool = False,
+    select: bool = True,
+    dry_run: bool = False,
+):
+    """Send a JSON or YAML file to the API to create a solution."""
 
-    if (converted_solution_content := get_api_file(api_file_path=solution_file,
-                                                   use_working_dir_file=use_solution_file,
-                                                   logger=logger)) is not None:
-        try:
-            if not dry_run:
-                r = solution_api.create_solution(organization_id=organization_id, solution=converted_solution_content)
-            else:
-                logger.info("DRY RUN - Would call solution_api.create_solution")
-                r = converted_solution_content
-                r['id'] = "<DRY RUN>"
-            logger.debug(pformat(r))
-            logger.info(f"Created new solution with id: {r['id']}")
-        except cosmotech_api.exceptions.UnauthorizedException as _e:
-            logger.error("Unauthorized access to the cosmotech api")
+    if dry_run:
+        logger.info("DRY RUN - Would call solution_api.create_solution")
+        return
+
+    converted_solution_content = get_api_file(
+        api_file_path=solution_file,
+        use_working_dir_file=use_working_dir_file,
+        logger=logger,
+    )
+    if not converted_solution_content:
+        logger.error("Error : can not get correct connector definition, please check your Solution.YAML file")
+        return
+
+    try:
+        retrieved_data = solution_api.create_solution(
+            organization_id=organization_id, solution=converted_solution_content
+        )
+    except UnauthorizedException:
+        logger.error("Unauthorized access to the cosmotech api")
+        return
+    except NotFoundException:
+        logger.error(f"Organization with id {organization_id} does not exists.")
+        return
+
+    logger.debug(pformat(retrieved_data))
+    logger.info(f"Created new solution with id: {retrieved_data['id']}")
