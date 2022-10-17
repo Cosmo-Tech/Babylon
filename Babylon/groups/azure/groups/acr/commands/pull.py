5bb6edd4ad6686da8e0ad887334dd528d7716fdb
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2022-10-17 10:27:07 +0200

    moved ACR registry to a platform key

--- Babylon/groups/azure/groups/acr/commands/pull.py
+++ Babylon/groups/azure/groups/acr/commands/pull.py
@@ -8,9 +8,8 @@ from click import Context
 from click import option
 from click import pass_context
 import docker
-from docker import errors as dock_errors
 
-from ......utils.decorators import require_deployment_key
+from ......utils.decorators import require_deployment_key, require_platform_key
 
 logger = logging.getLogger("Babylon")
 """Command Tests
@@ -25,14 +24,14 @@ Should add new entry to `docker image ls`
 
 @command()
 @pass_context
-@require_deployment_key("acr_src_registry_name", "acr_src_registry_name")
+@require_platform_key("acr_registry_name", "acr_registry_name")
 @require_deployment_key("acr_image_reference", "acr_image_reference")
 @option("-i", "--image", help="Local docker image to pull")
 @option("-r", "--registry", help="Container Registry name to pull from, example: myregistry.azurecr.io")
-def pull(ctx: Context, acr_src_registry_name: str, acr_image_reference: str, registry: typing.Optional[str],
+def pull(ctx: Context, acr_registry_name: str, acr_image_reference: str, registry: typing.Optional[str],
          image: typing.Optional[str]):
     """Pulls a docker image from the ACR registry given in deployment configuration"""
-    registry = registry or acr_src_registry_name
+    registry = registry or acr_registry_name
     image = image or acr_image_reference
     # Login to registry
     response = subprocess.run(["az", "acr", "login", "--name", registry],
@@ -50,7 +49,7 @@ def pull(ctx: Context, acr_src_registry_name: str, acr_image_reference: str, reg
     repo = f"{registry}/{image}"
     try:
         client.images.pull(repository=repo)
-    except dock_errors.NotFound:
+    except docker.errors.NotFound:
         logger.error(f"Registry {registry} does not contain {image}")
-    except dock_errors.APIError as api_error:
+    except docker.errors.APIError as api_error:
         logger.error(f"API Error: {api_error}")
