dba9f77ecc92ece12686afe3f324d38f95e21725
johnfolly <john.folly@cosmotech.com>
2022-10-18 10:02:26 +0200

    added workspace get-all implementation

--- Babylon/groups/api/groups/workspace/__init__.py
+++ Babylon/groups/api/groups/workspace/__init__.py
@@ -1,16 +1,22 @@
+import imp
 from click import group
 from click import pass_context
 from click.core import Context
+from cosmotech_api.api.workspace_api import WorkspaceApi
+from cosmotech_api import ApiClient
 
+from Babylon.utils.decorators import pass_api_configuration
 from .commands import list_commands
 from .groups import list_groups
 
 
 @group()
+@pass_api_configuration
 @pass_context
-def workspace(ctx: Context):
-    """Group initialized from a template"""
-    pass
+def workspace(ctx: Context, api_configuration):
+    """Subgroup handling Work in the cosmotech API"""
+    api_client = ApiClient(api_configuration)
+    ctx.obj = WorkspaceApi(api_client)
 
 
 for _command in list_commands:
