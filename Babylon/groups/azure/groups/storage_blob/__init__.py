fa2449c39f03fc70d6acaa2ce7d5e5d146635771
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2022-10-25 14:37:32 +0200

    Added storage account option to storage-blob group

--- Babylon/groups/azure/groups/storage_blob/__init__.py
+++ Babylon/groups/azure/groups/storage_blob/__init__.py
@@ -2,22 +2,25 @@ import logging
 from azure.storage.blob import BlobServiceClient
 from click import group
 from click import pass_context
+from click import option
 from click.core import Context
 
 from .commands import list_commands
 from .groups import list_groups
 from .....utils.decorators import require_deployment_key
 
-logger = logging.getLogger()
+logger = logging.getLogger("Babylon")
 
 
 @group()
+@option("-a", "--account", "account", help="Storage account name")
 @pass_context
 @require_deployment_key("storage_account_name", "storage_account_name")
-def storage_blob(ctx: Context, storage_account_name: str):
+def storage_blob(ctx: Context, account: str, storage_account_name: str):
     """Group interacting with Azure Storage Blob"""
-    logger.info(f"Connecting to storage account '{storage_account_name}'")
-    account_url = f"https://{storage_account_name}.blob.core.windows.net"
+    account_name = account or storage_account_name
+    logger.debug(f"Connecting to storage account '{account_name}'")
+    account_url = f"https://{account_name}.blob.core.windows.net"
     blob_client = BlobServiceClient(account_url, ctx.parent.obj)
     ctx.obj = blob_client
 
