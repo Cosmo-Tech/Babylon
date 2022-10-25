28af3f680733a95abe2a37b67433536a1707b865
Alexis Fossart <alexis.fossart@cosmotech.com>
2022-10-25 15:53:46 +0200

    Added tests_todo command in dev-tools
    With a gitref this command will list the commands that may need a manual test

--- plugins/dev_tools/commands/__init__.py
+++ plugins/dev_tools/commands/__init__.py
@@ -1,3 +1,4 @@
+from .tests_todo import tests_todo
 from .initialize_plugin import initialize_plugin
 from .list_required_keys import list_required_keys
 from .move_group import move_group
@@ -6,6 +7,7 @@ from .initialize_command import initialize_command
 from .initialize_group import initialize_group
 
 list_commands = [
+    tests_todo,
     initialize_plugin,
     list_required_keys,
     move_group,
