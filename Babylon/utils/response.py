225207d1b5aa14620e06226608473875c7e98d09
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2022-10-26 15:52:18 +0200

    Adding parameters

--- Babylon/utils/response.py
+++ Babylon/utils/response.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Any
 from typing import Optional
-
+from pprint import pformat
 from click import Command
 
 
@@ -16,11 +16,21 @@ class CommandResponse():
 
     def __init__(self,
                  command: Command,
+                 params: dict[str, Any],
                  status_code: Optional[CommandStatus] = CommandStatus.OK,
                  data: Optional[dict[str, Any]] = None) -> None:
         self.status_code = status_code
         self.data = data
-        self.command = command
+        self.command = command.name
+        self.params = params
 
     def to_dict(self) -> dict[str, Any]:
-        return {"command": self.command.name, "status_code": self.status_code, "data": self.data}
+        return {"command": self.command, "params": self.params, "status_code": self.status_code, "data": self.data}
+
+    def __str__(self) -> str:
+        return "\n".join([
+            f"Command: {self.command}", "\n".join([f"  -  {key}: {param}" for key, param in self.params.items()]),
+            f"Status code: {self.status_code}",
+            "Return value:",
+            pformat(self.data)
+        ])
