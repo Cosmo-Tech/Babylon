831cdefff13332c9926b8262782a98401ae488cc
Alexis Fossart <alexis.fossart@cosmotech.com>
2022-11-07 17:25:49 +0100

    Updated how_to_test_command to render markdown

--- plugins/dev_tools/groups/tests/commands/how_to_test_command.py
+++ plugins/dev_tools/groups/tests/commands/how_to_test_command.py
@@ -1,6 +1,8 @@
 import logging
 
 import click
+import rich.markdown
+import rich
 
 from Babylon.utils import BABYLON_PATH
 
@@ -12,10 +14,11 @@ logger = logging.getLogger("Babylon")
 def how_to_test_command(command: list[str]):
     """Will go through the test organization to display the tests of the given COMMAND"""
     p = BABYLON_PATH.parent / ("tests/commands/" + "/".join(command))
+    _c = rich.get_console()
     if not p.exists():
         logger.info(f"Command {' '.join(command)} has no tests defined")
         return
     logger.info(f"Test of command {' '.join(command)}")
+
     with p.open("r") as f:
-        for line in f:
-            logger.info("  " + line)
+        _c.print(rich.markdown.Markdown("".join(f.readlines())))
