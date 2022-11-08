de15ceaff5aa1a66d3cac3b72fa2bf700d8cf49b
John FOLLY <112887402+johnfolly@users.noreply.github.com>
2022-11-08 17:21:08 +0100

    Update Babylon/groups/api/groups/solution/commands/update.py
    Co-authored-by: Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>

--- Babylon/groups/api/groups/solution/commands/update.py
+++ Babylon/groups/api/groups/solution/commands/update.py
@@ -92,7 +92,7 @@ def update(
         logger.error("Unauthorized access to the cosmotech api")
         return
     except NotFoundException:
-        logger.error(f"Solution {solution_id} does not exists in organization {organization_id}.")
+        logger.error(f"Solution {solution_id} not found in organization {organization_id}.")
         return
     except ServiceException:
         logger.error(f"Organization with id {organization_id} not found.")
