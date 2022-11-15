e1702a9547b2a15f3b5a1f0f70d2f5e776c9e107
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2022-11-15 15:53:52 +0100

    Handling arguments

--- Babylon/commands/run.py
+++ Babylon/commands/run.py
@@ -18,8 +18,27 @@ def execute_step(root: click.Context, step: Dict[str, Any]):
         ["Babylon", "".join([f".groups.{group}" for group in commands[:-1]]), f".commands.{commands[-1]}"])
     command_module = import_module(to_import, "Babylon")
     cmd = getattr(command_module, commands[-1])
-    return root.invoke(cmd, **step.get("params", {}))
+    return root.invoke(cmd, *step.get("arguments", []), **step.get("options", {}))
 
+
+def _insert_from_forward(step: dict[str, Any], data: dict[str, Any]) -> dict[str, Any]:
+    # Inserting values for arguments
+    arguments = step.get("arguments", [])
+    for idx, argument in enumerate(arguments):
+        needle = re.match(r"\$\w+\$", argument)
+        if not needle:
+            continue
+        fwd_k = needle.group().replace("$", "")
+        arguments[idx] = data[fwd_k]
+    # Inserting values for options
+    options = step.get("options", {})
+    for key, option in options.items():
+        needle = re.match(r"\$\w+\$", option)
+        if not needle:
+            continue
+        fwd_k = needle.group().replace("$", "")
+        options[key] = data[fwd_k]
+    return {**step, "arguments": arguments, "options": options}
 
 @click.command
 @click.pass_context
@@ -35,18 +54,8 @@ def run(ctx: click.Context, script: str):
     root = ctx.find_root()
     forward_data = {}
     for step in macro.get("steps", []):
-
-        # Insert values from forward in parameters
-        for key, param in step.get("params", {}).items():
-            needle = re.match(r"\$\w+\$", param)
-            if not needle:
-                continue
-            fwd_k = needle.group().replace("$", "")
-            # Replacing variable symbol by its actual value
-            step["params"][key] = forward_data[fwd_k]
-
+        step = _insert_from_forward(step, forward_data)
         response: CommandResponse = execute_step(root, step)
-
         # Forwarding response data to next commands
         for fwd_k, data_k in step.get("forward", {}).items():
             forward_data[fwd_k] = jmespath.search(data_k, response.data)
