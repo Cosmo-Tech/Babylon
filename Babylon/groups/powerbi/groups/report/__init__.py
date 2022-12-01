f2660a669068b4e23048d280b71344ba96649940
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2022-12-01 14:48:03 +0100

    initialize empty groups and powerbi credentials

---
+++ Babylon/groups/powerbi/groups/report/__init__.py
@@ -0,0 +1,20 @@
+from click import group
+from click import pass_context
+from click.core import Context
+
+from .commands import list_commands
+from .groups import list_groups
+
+
+@group()
+@pass_context
+def report(ctx: Context):
+    """Group initialized from a template"""
+    pass
+
+
+for _command in list_commands:
+    report.add_command(_command)
+
+for _group in list_groups:
+    report.add_command(_group)
