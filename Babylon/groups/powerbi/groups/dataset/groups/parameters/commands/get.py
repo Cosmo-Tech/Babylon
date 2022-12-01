a8c256149ee3f7ad1a513c7b2043265685a3311d
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2022-12-01 15:54:24 +0100

    Added commandresponse returns

--- Babylon/groups/powerbi/groups/dataset/groups/parameters/commands/get.py
+++ Babylon/groups/powerbi/groups/dataset/groups/parameters/commands/get.py
@@ -7,6 +7,8 @@ from click import pass_context
 from click import Context
 
 from ........utils.decorators import require_deployment_key
+from ........utils.response import CommandResponse
+
 
 logger = logging.getLogger("Babylon")
 
@@ -15,10 +17,15 @@ logger = logging.getLogger("Babylon")
 @pass_context
 @require_deployment_key("powerbi_workspace_id")
 @argument("dataset_id")
-def get(ctx: Context, powerbi_workspace_id: str, dataset_id: str):
+def get(ctx: Context, powerbi_workspace_id: str, dataset_id: str) -> CommandResponse:
     """Get parameters of a given dataset"""
     access_token = ctx.obj.token
     header = {'Content-Type': 'application/json', 'Authorization': f'Bearer {access_token}'}
     update_url = f"https://api.powerbi.com/v1.0/myorg/groups/{powerbi_workspace_id}/datasets/{dataset_id}/parameters"
-    response = requests.get(url=update_url, headers=header)
+    try:
+        response = requests.get(url=update_url, headers=header)
+    except Exception as e:
+        logger.error(f"Request failed {e}")
+        return CommandResponse(status_code=CommandResponse.STATUS_ERROR)
     logger.info(response.json())
+    return CommandResponse(data=response.json())
--- Babylon/groups/powerbi/groups/workspace/commands/get.py
+++ Babylon/groups/powerbi/groups/workspace/commands/get.py
@@ -6,6 +6,8 @@ from click import option
 from click import pass_context
 from click import Context
 
+from ......utils.response import CommandResponse
+
 logger = logging.getLogger("Babylon")
 
 
@@ -13,11 +15,16 @@ logger = logging.getLogger("Babylon")
 @pass_context
 @option("-i", "--id", "id")
 @option("-n", "--name", "name")
-def get(ctx: Context, id: str, name: str):
+def get(ctx: Context, id: str, name: str) -> CommandResponse:
     """Get a specific workspace information"""
     url_groups = 'https://api.powerbi.com/v1.0/myorg/groups'
     access_token = ctx.obj.token
     header = {'Content-Type': 'application/json', 'Authorization': f'Bearer {access_token}'}
     params = {"$filter": f"id eq'{id}'"} if id else {"$filter": f"name eq '{name}'"}
-    response = requests.get(url=url_groups, headers=header, params=params)
+    try:
+        response = requests.get(url=url_groups, headers=header, params=params)
+    except Exception as e:
+        logger.error(f"Request failed {e}")
+        return CommandResponse(status_code=CommandResponse.STATUS_ERROR)
     logger.info(response.json())
+    return CommandResponse(data=response.json())
