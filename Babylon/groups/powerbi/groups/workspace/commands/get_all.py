b1c68957a748ff779334be522ce013b1b71525c2
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2022-12-01 14:14:20 +0100

    WIP new commands - powerbi workspage get - powerbi workspace get-all - powerbi report upload - powerbi report download - powerbi report get-all

--- Babylon/groups/powerbi/groups/workspace/commands/get_all.py
+++ Babylon/groups/powerbi/groups/workspace/commands/get_all.py
@@ -1,11 +1,18 @@
 import logging
-
 from click import command
+from click import pass_context
+from click import Context
+import requests
 
 logger = logging.getLogger("Babylon")
 
 
 @command()
-def get_all():
-    """Command created from a template"""
-    logger.warning("This command was initialized from a template and is empty")
+@pass_context
+def get_all(ctx: Context):
+    """Get all workspace information for the given account"""
+    url_groups = 'https://api.powerbi.com/v1.0/myorg/groups'
+    access_token = ctx.obj.token
+    header = {'Content-Type': 'application/json', 'Authorization': f'Bearer {access_token}'}
+    api_out = requests.get(url=url_groups, headers=header)
+    logger.info(api_out.json())
