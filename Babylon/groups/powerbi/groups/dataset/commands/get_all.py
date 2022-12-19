573b32e5fcd8b9c814aeabc973a34ec2cef08437
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2022-12-19 10:36:48 +0100

    improved get_all output formatting

--- Babylon/groups/powerbi/groups/dataset/commands/get_all.py
+++ Babylon/groups/powerbi/groups/dataset/commands/get_all.py
@@ -28,5 +28,6 @@ def get_all(ctx: Context, powerbi_workspace_id: str, workspace_id: Optional[str]
     except Exception as e:
         logger.error(f"Request failed {e}")
         return CommandResponse(status_code=CommandResponse.STATUS_ERROR)
-    logger.info(response.json().get("value"))
-    return CommandResponse(data=response.json())
+    output_data = response.json().get("value")
+    logger.info("\n".join([str(data) for data in output_data]))
+    return CommandResponse(data=output_data)
--- Babylon/groups/powerbi/groups/report/commands/get_all.py
+++ Babylon/groups/powerbi/groups/report/commands/get_all.py
@@ -28,6 +28,6 @@ def get_all(ctx: Context, powerbi_workspace_id: str, workspace_id: Optional[str]
     except Exception as e:
         logger.error(f"Request failed {e}")
         return CommandResponse(status_code=CommandResponse.STATUS_ERROR)
-    output_val = response.json().get("value")
-    logger.info(output_val)
-    return CommandResponse(data=output_val)
+    output_data = response.json().get("value")
+    logger.info("\n".join([str(data) for data in output_data]))
+    return CommandResponse(data=output_data)
