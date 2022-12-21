80f26fa033247b39a57f161058d105c5f88f2e86
Alexis Fossart <alexis.fossart@cosmotech.com>
2022-12-21 17:09:55 +0100

    First draft of data storage in Environment

--- Babylon/utils/environment.py
+++ Babylon/utils/environment.py
@@ -1,5 +1,9 @@
+import json
 import os
 import pathlib
+from collections import defaultdict
+from typing import Any
+from typing import List
 from typing import Optional
 
 import click
@@ -32,14 +36,69 @@ class Environment(metaclass=SingletonMeta):
     configuration: Optional[Configuration] = None
     working_dir: Optional[WorkingDir] = None
 
+    data_store = None
+
     def __init__(self):
         self.dry_run = False
 
         self.set_configuration(pathlib.Path(os.environ.get('BABYLON_CONFIG_DIRECTORY', click.get_app_dir("babylon"))))
         self.set_working_dir(pathlib.Path(os.environ.get('BABYLON_WORKING_DIRECTORY', ".")))
+        self.reset_data_store()
 
     def set_configuration(self, configuration_path: pathlib.Path):
         self.configuration = Configuration(config_directory=configuration_path)
 
     def set_working_dir(self, working_dir_path: pathlib.Path):
         self.working_dir = WorkingDir(working_dir_path=working_dir_path)
+
+    def reset_data_store(self):
+
+        def _nested_dict():
+            return defaultdict(_nested_dict)
+
+        self.data_store = _nested_dict()
+
+    def store_data(self, data_path: List[str], value: Any) -> bool:
+        """
+        Will use a data path composed of multiple keys to insert data in the store
+        :param data_path: succession of keys to be the target to store the data
+        :param value: the element to be stored
+        :return: Was the element successfully stored ?
+        """
+        if not len(data_path):
+            raise KeyError("Require at least one component for the data path")
+
+        current_store = self.data_store
+
+        *intermediate_keys, final_key = data_path
+
+        for key in intermediate_keys:
+            current_store = current_store[key]
+            if not isinstance(current_store, dict):
+                raise KeyError("Data path is incorrect")
+
+        current_store[final_key] = value
+
+        return True
+
+    def get_data(self, data_path: List[str]) -> Any:
+        """
+        Will look up a data from a given data path and return it
+        :param data_path: the path to the data to get
+        :return: the value of the data or None if it is not existent
+        """
+        if not len(data_path):
+            raise KeyError("Require at least one component for the data path")
+        current_store = self.data_store
+
+        *intermediate_keys, final_key = data_path
+
+        for key in intermediate_keys:
+            current_store = current_store.get(key)
+            if not isinstance(current_store, dict):
+                raise KeyError("Data path is incorrect")
+
+        return current_store.get(final_key)
+
+    def printable_store(self) -> str:
+        return json.dumps(self.data_store, indent=2, default=str)
--- tests/utils/test_environment.py
+++ tests/utils/test_environment.py
@@ -1,3 +1,5 @@
+import pytest
+
 from Babylon.utils.environment import Environment
 
 
@@ -6,3 +8,94 @@ def test_init():
     env = Environment()
     assert env.configuration
     assert env.working_dir
+
+
+def test_datastore_store_valid_key():
+    """Test if storing data with a valid key works"""
+    env = Environment()
+    env.reset_data_store()
+
+    path = ['SimpleKey', ]
+    value = "Value"
+
+    assert env.store_data(path, value)
+
+
+def test_datastore_store_valid_composite_key():
+    """Test if storing data using a composite key works"""
+    env = Environment()
+    env.reset_data_store()
+
+    path = ['Composite', 'Key']
+    value = "Value"
+
+    assert env.store_data(path, value)
+
+
+def test_set_with_no_path_into_storage():
+    """Test that a key is required to send data into storage"""
+    env = Environment()
+    env.reset_data_store()
+    path = []
+    value = "Value"
+
+    with pytest.raises(KeyError):
+        env.store_data(path, value)
+
+
+def test_datastore_refuse_key_over_existing():
+    """Test if storing data over already existing data is correctly blocked"""
+    env = Environment()
+    env.reset_data_store()
+    path = ['Key']
+    value = "Value"
+    assert env.store_data(path, value)
+    path = ['Key', 'Key2']
+    value = "Value"
+
+    with pytest.raises(KeyError):
+        env.store_data(path, value)
+
+
+def test_datastore_composite_keys_stores():
+    """Test that sub-keys can be added"""
+    env = Environment()
+    env.reset_data_store()
+    path = ['Key', 'Key1']
+    value = "Value"
+    assert env.store_data(path, value)
+    path = ['Key', 'Key2']
+    value = "Value"
+
+    assert env.store_data(path, value)
+
+
+def test_get_data_from_storage():
+    """Test to get back data from storage"""
+    env = Environment()
+    env.reset_data_store()
+    path = ['Key', 'Key1']
+    value = "Value"
+    assert env.store_data(path, value)
+
+    assert env.get_data(path) == value
+
+
+def test_get_non_existing_data_from_storage():
+    """Test that non-existing key from storage does not return"""
+    env = Environment()
+    env.reset_data_store()
+    path = ['Key', 'Key1']
+
+    with pytest.raises(KeyError):
+        env.get_data(path)
+
+
+def test_get_with_no_path_from_storage():
+    """Test that a key is required to get data from storage"""
+    env = Environment()
+    env.reset_data_store()
+    path = []
+
+    with pytest.raises(KeyError):
+        env.get_data(path)
