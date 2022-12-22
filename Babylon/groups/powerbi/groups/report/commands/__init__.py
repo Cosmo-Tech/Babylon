04a03038a8d3ca4c10547c6f79da2c54aaba0239
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2022-12-22 19:42:41 +0100

    added report get command

--- Babylon/groups/powerbi/groups/report/commands/__init__.py
+++ Babylon/groups/powerbi/groups/report/commands/__init__.py
@@ -1,8 +1,10 @@
+from .get import get
 from .get_all import get_all
 from .download import download
 from .upload import upload
 
 list_commands = [
+    get,
     download,
     upload,
     get_all,
