77ae8f4b08e27a6a0730ec1e2ecea7f1e8713842
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2023-01-02 14:38:09 +0100

    Added commands to set configuration variables

--- Babylon/groups/config/groups/deployment/commands/__init__.py
+++ Babylon/groups/config/groups/deployment/commands/__init__.py
@@ -1,8 +1,10 @@
+from .set_variable import set_variable
 from .create import create
 from .edit import edit
 from .select import select
 
 list_commands = [
+    set_variable,
     create,
     edit,
     select,
