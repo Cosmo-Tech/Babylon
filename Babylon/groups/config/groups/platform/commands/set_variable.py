d42963211d21c802f3c8347a05c8a5a505eee205
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2023-01-04 14:13:28 +0100

    Add command response success on new config cmds

--- Babylon/groups/config/groups/platform/commands/set_variable.py
+++ Babylon/groups/config/groups/platform/commands/set_variable.py
@@ -16,4 +16,4 @@ def set_variable(variable_key: str, variable_value: str):
     """Set a platform variable with a new value"""
     env = Environment()
     env.configuration.set_platform_var(variable_key, variable_value)
-    return CommandResponse()
+    return CommandResponse.success()
