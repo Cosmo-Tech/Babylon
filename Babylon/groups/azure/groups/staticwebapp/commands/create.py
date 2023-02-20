d2cb99046e7b590ace794892b78c5c4b98299d23
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2023-02-20 14:22:11 +0100

    added polling to wait for swa creation

--- Babylon/groups/azure/groups/staticwebapp/commands/create.py
+++ Babylon/groups/azure/groups/staticwebapp/commands/create.py
@@ -13,7 +13,9 @@ from rich.pretty import pretty_repr
 
 from ......utils.environment import Environment
 from ......utils.request import oauth_request
+from ......utils.request import poll_request
 from ......utils.decorators import require_platform_key
+from ......utils.decorators import require_deployment_key
 from ......utils.response import CommandResponse
 
 logger = logging.getLogger("Babylon")
@@ -23,6 +25,8 @@ logger = logging.getLogger("Babylon")
 @pass_context
 @require_platform_key("azure_subscription")
 @require_platform_key("resource_group_name")
+@require_deployment_key("webapp_repository")
+@require_deployment_key("webapp_repository_branch")
 @argument("webapp_name")
 @option("-f", "--file", "create_file", type=Path(readable=True, dir_okay=False, path_type=pathlib.Path), required=True)
 @option("-e",
@@ -30,12 +34,16 @@ logger = logging.getLogger("Babylon")
         "use_working_dir_file",
         is_flag=True,
         help="Should the parameter file path be relative to Babylon working directory ?")
+@option("-w", "--wait", "wait", is_flag=True, help="Should the command return after resource has been created ?")
 def create(ctx: Context,
            azure_subscription: str,
            resource_group_name: str,
+           webapp_repository: str,
+           webapp_repository_branch: str,
            webapp_name: str,
            create_file: str,
-           use_working_dir_file: bool = False) -> CommandResponse:
+           use_working_dir_file: bool = False,
+           wait: bool = False) -> CommandResponse:
     """
     Create a static webapp data in the given resource group
     https://learn.microsoft.com/en-us/rest/api/appservice/static-sites/create-or-update-static-site
@@ -61,4 +69,14 @@ def create(ctx: Context,
     output_data = response.json()
     logger.info(pretty_repr(output_data))
     logger.info(f"Successfully launched creation of webapp {webapp_name} in resource group {resource_group_name}")
+    if not wait:
+        return CommandResponse.success(output_data)
+    # Waiting for github action to be created
+    hostname = output_data["properties"]["defaultHostname"].split(".")[0]
+    response = poll_request(20,
+                            True,
+                            url=f"{webapp_repository}/blob/{webapp_repository_branch}/.github/workflows/azure-static-web-apps-{hostname}.yml",
+                            access_token="")
+    if response is None:
+        return CommandResponse.fail()
     return CommandResponse.success(output_data)
