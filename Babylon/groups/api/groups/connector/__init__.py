8de306517aeeafc9392c42719778b8f2a9c7dabf
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2023-02-23 15:21:09 +0100

    changed api client usage in api connectors

--- Babylon/groups/api/groups/connector/__init__.py
+++ Babylon/groups/api/groups/connector/__init__.py
@@ -1,21 +1,13 @@
-import cosmotech_api
 from click import group
-from click import pass_context
-from click.core import Context
-from cosmotech_api.api.connector_api import ConnectorApi
 
-from .....utils.decorators import pass_api_configuration
 from .commands import list_commands
 from .groups import list_groups
 
 
 @group()
-@pass_api_configuration
-@pass_context
-def connector(ctx: Context, api_configuration):
+def connector():
     """Subgroup handling Connectors in the cosmotech API"""
-    api_client = cosmotech_api.ApiClient(api_configuration)
-    ctx.obj = ConnectorApi(api_client)
+    pass
 
 
 for _command in list_commands:
