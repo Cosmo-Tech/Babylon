e39c31e7827aa60fb433c7c76ae8aa787770d465
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2023-03-06 12:15:57 +0100

    Added missing query type in azure ad app delete

--- Babylon/groups/azure/groups/ad/groups/app/commands/delete.py
+++ Babylon/groups/azure/groups/ad/groups/app/commands/delete.py
@@ -8,13 +8,14 @@ from ........utils.interactive import confirm_deletion
 from ........utils.request import oauth_request
 from ........utils.response import CommandResponse
 from ........utils.credentials import pass_azure_token
+from ........utils.typing import QueryType
 
 logger = logging.getLogger("Babylon")
 
 
 @command()
 @pass_azure_token("graph")
-@argument("registration_id")
+@argument("registration_id", type=QueryType())
 @option("-f", "--force", "force_validation", is_flag=True, help="Don't ask for validation before delete")
 def delete(azure_token: str, registration_id: str, force_validation: bool = False) -> CommandResponse:
     """
