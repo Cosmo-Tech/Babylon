204e6c5024acc7ee90458d779c76963d479a3fc1
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2023-03-13 16:10:14 +0100

    Updated connector create

--- Babylon/commands/api/connector/create.py
+++ Babylon/commands/api/connector/create.py
@@ -1,10 +1,10 @@
-import json
 from logging import getLogger
 from pprint import pformat
 from typing import Optional
+import pathlib
 
-from click import Choice
 from click import Path
+from click import Choice
 from click import argument
 from click import command
 from click import option
@@ -22,15 +22,22 @@ from ....utils.typing import QueryType
 from ....utils.response import CommandResponse
 from ....utils.clients import pass_api_client
 from ....utils.decorators import output_to_file
+from ....utils.api import camel_to_underscore
+from ....utils.environment import Environment
 
 logger = getLogger("Babylon")
+
+DEFAULT_PAYLOAD_TEMPLATES = {
+    "ADT": ".payload_templates/api/Connector.ADT.yaml",
+    "STORAGE": ".payload_templates/api/Connector.STORAGE.yaml"
+}
 
 
 @command()
 @describe_dry_run("Would call **connector_api.create_connector** to register a new Connector")
 @timing_decorator
 @pass_api_client
-@option("-i", "--connector-file", "connector_file", type=str, help="Your custom Connector description file path")
+@option("-i", "--connector-file", "connector_file", type=Path(readable=True, dir_okay=False, path_type=pathlib.Path))
 @option("-t",
         "--type",
         "connector_type",
@@ -39,30 +46,26 @@ logger = getLogger("Babylon")
         help="Connector type, allowed values : [ADT, STORAGE]")
 @argument("connector-name", type=QueryType())
 @option("-v", "--version", "connector_version", required=True, help="Version of the Connector")
+@output_to_file
 def create(
     api_client: ApiClient,
     connector_type: str,
     connector_name: str,
     connector_version: str,
     output_file: Optional[str] = None,
-    connector_file: Optional[str] = None
+    connector_file: Optional[pathlib.Path] = None
 ) -> CommandResponse:
     """Register a new Connector by sending a JSON or YAML file to the API."""
     connector_api = ConnectorApi(api_client)
     connector_type = connector_type.upper()
-    converted_connector_content = get_api_file(
-        api_file_path=connector_file
-        if connector_file else f"{TEMPLATE_FOLDER_PATH}/working_dir_template/API/Connector.{connector_type}.yaml")
-    if not converted_connector_content:
-        logger.error("Error : can not get correct Connector definition, please check your Connector.YAML file")
-        return CommandResponse.fail()
-
-    converted_connector_content["name"] = connector_name
-    converted_connector_content["version"] = connector_version
-    if not converted_connector_content.get("key"):
-        converted_connector_content["key"] = connector_name.replace(" ", "")
-    if not converted_connector_content.get("description"):
-        converted_connector_content["description"] = connector_name
+    connector_content = connector_file or DEFAULT_PAYLOAD_TEMPLATES[connector_type]
+    env = Environment()
+    converted_connector_content = convert_keys_case(env.fill_template(connector_content, data={
+        "connector_name": connector_name,
+        "connector_version": connector_version,
+        "connector_key": connector_name.replace(" ", ""),
+        "connector_description": connector_name
+    }), camel_to_underscore)
 
     try:
         retrieved_connector = connector_api.register_connector(connector=converted_connector_content)
@@ -70,9 +73,8 @@ def create(
         logger.error("Unauthorized access to the cosmotech api")
         return CommandResponse.fail()
 
-    logger.debug(pformat(retrieved_connector))
-    logger.info("Created new %s Connector with id: %s", connector_type, retrieved_connector['id'])
-
     converted_connector_content = convert_keys_case(retrieved_connector, underscore_to_camel)
+    logger.debug(pformat(converted_connector_content))
+    logger.info("Created new %s Connector with id: %s", connector_type, converted_connector_content['id'])
 
     return CommandResponse.success(converted_connector_content)
