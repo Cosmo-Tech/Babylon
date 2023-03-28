c1b0f623c4f793fbde14b8d7889e64804a2f4dac
Emmanuel <emmanuel.ruaud.ext@cosmotech.com>
2023-03-28 10:46:55 +0200

    Disable workspace creation if already existing

--- Babylon/commands/powerbi/deploy_workspace.py
+++ Babylon/commands/powerbi/deploy_workspace.py
@@ -32,18 +32,23 @@ logger = logging.getLogger("Babylon")
 def deploy_workspace(workspace_name: str,
                      report_folder: pathlib.Path,
                      report_parameters: Optional[Iterable[Tuple[str, str]]] = None):
-    """Macro command allowing full deployment of a power bi workspac
-
-    Require a local folder named `powerbi-reports` and will initialize a full workspace with the given reports"""
+    """
+    Macro command allowing full deployment of a power bi workspac
+    Require a local folder named `powerbi-reports` and will initialize a full workspace with the given reports
+    Won't run powerbi workspace creation if a workspace id is given
+    """
     report_params = " ".join([f"-p {param[0]} {param[1]}" for param in report_parameters]) if report_parameters else ""
 
-    macro: Macro = Macro("powerbi workspace deploy") \
-        .step(["powerbi", "workspace", "create", workspace_name], store_at="workspace")
+    macro = Macro("powerbi workspace deploy") \
+        .step(["powerbi", "workspace", "get", "-n", workspace_name], store_at="workspace", is_required=False)
+    macro.step(["powerbi", "workspace", "create", workspace_name],
+               store_at="workspace",
+               run_if=not macro.env.get_data(["workspace", "data"]))
     for report_path in report_folder.glob("*.pbix"):
         macro.step(["powerbi", "report", "upload", "-w", "%datastore%workspace.data.id",
                     str(report_path)],
                    store_at="report")
-        for dataset in macro.env.convert_data_query("%datastore%report.data.datasets"):
+        for dataset in macro.env.convert_data_query("%datastore%report.data.datasets") or []:
             macro.step(["powerbi", "dataset", "parameters", "update", "-w", "%datastore%workspace.data.id",
                        dataset["id"], *report_params.split(" ")], run_if=report_params != "") \
                 .step(["powerbi", "dataset", "update-credentials", "-w", "%datastore%workspace.data.id", dataset["id"]])
