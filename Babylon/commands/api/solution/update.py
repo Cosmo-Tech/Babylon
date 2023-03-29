136e21b66b350469360b4709bcf0582f4b22ed3a
John FOLLY <112887402+johnfolly@users.noreply.github.com>
2023-03-29 16:02:20 +0200

    Update Babylon/commands/api/solution/update.py
    Co-authored-by: Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>

--- Babylon/commands/api/solution/update.py
+++ Babylon/commands/api/solution/update.py
@@ -23,7 +23,7 @@ logger = getLogger("Babylon")
 @timing_decorator
 @require_platform_key("api_url")
 @pass_azure_token("csm_api")
-@argument("solution_id", type=QueryType(), default="%deploy%solution_id")
+@argument("solution_id", type=QueryType())
 @option("--organization", "organization_id", type=QueryType(), default="%deploy%organization_id")
 @option(
     "-i",
