2d34d115229621cdea2cf948ef2f6c623c10f2df
johnfolly <john.folly@cosmotech.com>
2023-04-12 10:38:52 +0200

    add filter to terraform-cloud workspace get-all

--- Babylon/commands/terraform_cloud/workspace/get_all.py
+++ Babylon/commands/terraform_cloud/workspace/get_all.py
@@ -1,12 +1,15 @@
 import logging
+from typing import Optional
 
+import jmespath
 from click import command
+from click import option
 from terrasnek.api import TFC
 
-from ....utils.decorators import timing_decorator
-from ....utils.response import CommandResponse
 from ....utils.clients import pass_tfc_client
 from ....utils.decorators import output_to_file
+from ....utils.decorators import timing_decorator
+from ....utils.response import CommandResponse
 
 logger = logging.getLogger("Babylon")
 
@@ -14,8 +17,9 @@ logger = logging.getLogger("Babylon")
 @command()
 @pass_tfc_client
 @output_to_file
+@option("--filter", "filter", help="Filter response with a jmespath query")
 @timing_decorator
-def get_all(tfc_client: TFC) -> CommandResponse:
+def get_all(tfc_client: TFC, filter: Optional[str] = None) -> CommandResponse:
     """Get all available workspaces in the organization"""
     ws = tfc_client.workspaces.list()
 
@@ -23,4 +27,6 @@ def get_all(tfc_client: TFC) -> CommandResponse:
         return _r.get('attributes', {}).get('latest-change-at', '')
 
     workspaces = [_ws for _ws in sorted(ws.get('data'), key=get_last_changed)]
+    if filter:
+        workspaces = jmespath.search(filter, workspaces)
     return CommandResponse.success({"workspaces": workspaces}, verbose=True)
