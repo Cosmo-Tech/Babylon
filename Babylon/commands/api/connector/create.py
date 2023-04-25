de290146044b5e7951882a1914941731558f4c71
enocquet <emmanuelnocquet8@gmail.com>
2023-04-25 17:01:06 +0200

    add an alternative way to define key names for connector id

--- Babylon/commands/api/connector/create.py
+++ Babylon/commands/api/connector/create.py
@@ -19,6 +19,7 @@ from ....utils.typing import QueryType
 from ....utils.yaml_utils import yaml_to_json
 
 logger = logging.getLogger("Babylon")
+env = Environment()
 
 
 @command()
@@ -39,17 +40,25 @@ logger = logging.getLogger("Babylon")
     is_flag=True,
     help="Select this new connector in configuration ?",
 )
+@option("-k",
+        "--key-name",
+        "key_name",
+        required=False,
+        type=str,
+        help="the key_name of this connector ('adt_connector_id', 'storage_connector_id')")
 @output_to_file
-def create(api_url: str,
-           azure_token: str,
-           connector_name: str,
-           connector_file: Optional[pathlib.Path] = None,
-           select: bool = False) -> CommandResponse:
+def create(
+    api_url: str,
+    azure_token: str,
+    connector_name: str,
+    key_name: str,
+    connector_file: Optional[pathlib.Path] = None,
+    select: bool = False,
+) -> CommandResponse:
     """
     Register a new Connector by sending a file to the API.
     See the API files to edit your own file manually
     """
-    env = Environment()
     details = env.fill_template(connector_file,
                                 data={
                                     "connector_name": connector_name,
@@ -62,9 +71,18 @@ def create(api_url: str,
         return CommandResponse.fail()
     connector = response.json()
     logger.info(f"Created new connector with id: {connector['id']}")
-    if select:
-        value = click.prompt("Select the key_name of this connector: ",
-                             type=click.Choice(["adt_connector_id", "storage_connector_id"]))
-        logger.info(f"Updated configuration variables with {value}")
-        env.configuration.set_deploy_var(value, connector["id"])
+
+    if key_name:
+        _handle_configuration_key_choice(key_name, connector)
+    elif select:
+        _handle_configuration_key_choice(key_name, connector)
+
     return CommandResponse.success(connector, verbose=True)
+
+
+def _handle_configuration_key_choice(key_name, connector):
+    if key_name and key_name not in ["adt_connector_id", "storage_connector_id"]:
+        key_name = click.prompt("Select the key_name of this connector: ",
+                                type=click.Choice(["adt_connector_id", "storage_connector_id"]))
+    logger.info(f"Updated configuration variables with {key_name}")
+    env.configuration.set_deploy_var(key_name, connector["id"])
