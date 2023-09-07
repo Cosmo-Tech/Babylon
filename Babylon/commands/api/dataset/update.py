9bd1e5a2e6e52fe03bc9fdd4b548aa553ada2e51
johnfolly <john.folly@cosmotech.com>
2023-09-07 16:30:25 +0200

    --dataset-id

--- Babylon/commands/api/dataset/update.py
+++ Babylon/commands/api/dataset/update.py
@@ -22,7 +22,7 @@ logger = getLogger("Babylon")
 @timing_decorator
 @require_platform_key("api_url")
 @pass_azure_token("csm_api")
-@option("--dataset", "dataset_id", type=QueryType(), default="%deploy%dataset_id")
+@option("--dataset-id", "dataset_id", type=QueryType(), default="%deploy%dataset_id")
 @argument("dataset_file", type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True, path_type=pathlib.Path))
 @option("--organization", "organization_id", type=QueryType(), default="%deploy%organization_id")
 @output_to_file
