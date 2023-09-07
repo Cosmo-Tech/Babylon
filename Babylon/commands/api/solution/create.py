b7147ba968885171966ebfc6fc651c76d57ce0c3
johnfolly <john.folly@cosmotech.com>
2023-09-07 16:51:47 +0200

    path import

--- Babylon/commands/api/solution/create.py
+++ Babylon/commands/api/solution/create.py
@@ -5,6 +5,7 @@ from typing import Optional
 from click import argument
 from click import command
 from click import option
+from click import Path
 
 from ....utils.credentials import pass_azure_token
 from ....utils.decorators import output_to_file
