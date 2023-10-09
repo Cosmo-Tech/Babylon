17d74efcc1d7b9b5c8a2bd3a9a913f4703afdae2
nibaldo <nibaldo.donoso@cosmotech.com>
2023-10-09 15:49:45 +0200

    add init script policies

--- Babylon/commands/azure/adx/database/create.py
+++ Babylon/commands/azure/adx/database/create.py
@@ -1,7 +1,8 @@
+import json
 import logging
 
 from typing import Any, Optional
-from click import Context, argument, command, option, pass_context
+from click import Context, argument, command, option, pass_context, progressbar
 from azure.mgmt.kusto import KustoManagementClient
 from azure.mgmt.kusto.models import ReadWriteDatabase
 from azure.mgmt.kusto.models import CheckNameRequest
@@ -79,12 +80,42 @@ def create(ctx: Context,
     if not poller.done():
         return CommandResponse.fail()
     # batabase created
+
+    # init bd with policies
+    script_name = f"initdb-{name}.kusto"
+    batching_policy = json.dumps({"MaximumBatchingTimeSpan": "00:00:10"})
+    # script_content = f".alter database ['{name}'] policy streamingingestion disable\n"
+    # script_content += "//\n"
+    script_content = f".alter database ['{name}'] policy ingestionbatching '{batching_policy}'"
+    s = kusto_client.scripts.begin_create_or_update(resource_group_name=resource_group_name,
+                                                    cluster_name=adx_cluster_name,
+                                                    database_name=name,
+                                                    content_type="application/json",
+                                                    script_name=script_name,
+                                                    parameters={"script_content": script_content},
+                                                    polling_interval=1)
+    try:
+        with progressbar(length=20, label="Waiting for init script to finish") as bar:
+            for _ in bar:
+                if not s.done():
+                    s.wait(1)
+            while not s.done():
+                s.wait(1)
+    except Exception as _resp_error:
+        logger.error(_resp_error.message.split("\nMessage:")[1])
+        return CommandResponse.fail()
+    kusto_client.scripts.begin_delete(
+        resource_group_name=resource_group_name,
+        cluster_name=adx_cluster_name,
+        database_name=name,
+        script_name=script_name,
+    )
+    logger.info("Successfully ran")
     if select:
         env.configuration.set_var(resource_id=ctx.parent.parent.command.name,
                                   var_name="database_name",
                                   var_value=name.lower())
         logger.info(SUCCESS_CONFIG_UPDATED("adx", "database_name"))
-
     _ret: list[str] = [f"Provisioning state: {poller.result().provisioning_state}"]
     logger.info("\n".join(_ret))
     return CommandResponse.success()
