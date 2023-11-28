42b89c355e369460ec15902c21f9df653f03b81b
nibaldo <nibaldo.donoso@cosmotech.com>
2023-11-28 14:00:32 +0100

    fix upload and donwload handler

--- Babylon/commands/api/solutions/handler/download.py
+++ Babylon/commands/api/solutions/handler/download.py
@@ -1,12 +1,16 @@
+import os
+import zipfile
+
+from posixpath import basename
 from logging import getLogger
 from typing import Any
 from click import command
 from click import option
 from click import argument
+from azure.storage.blob import BlobServiceClient
+from Babylon.utils.clients import pass_blob_client
 from Babylon.utils.decorators import inject_context_with_resource, timing_decorator, wrapcontext
 from Babylon.utils.response import CommandResponse
-from Babylon.utils.credentials import pass_azure_token
-from Babylon.utils.request import oauth_request
 from Babylon.utils.typing import QueryType
 from Babylon.utils.environment import Environment
 from Babylon.utils.decorators import output_to_file
@@ -17,23 +21,35 @@ env = Environment()
 
 @command()
 @wrapcontext()
+@pass_blob_client
 @timing_decorator
 @output_to_file
-@pass_azure_token("csm_api")
 @option("--organization-id", "org_id", type=QueryType(), help="Organization id or referenced in configuration")
 @option("--solution-id", "sol_id", type=QueryType(), help="Solution id or referenced in configuration")
 @option("--run-template", "run_template_id", help="The run Template identifier", required=True, type=QueryType())
 @argument("handler_id", type=QueryType())
 @inject_context_with_resource({'api': ['url', 'organization_id', "solution_id"]})
-def download(context: Any, azure_token: str, org_id: str, sol_id: str, handler_id: str,
-             run_template_id: str) -> CommandResponse:
+def download(
+    blob_client: BlobServiceClient,
+    context: Any,
+    org_id: str,
+    sol_id: str,
+    handler_id: str,
+    run_template_id: str
+) -> CommandResponse:
     """Download a solution handler zip from the solution"""
-    organization_id = org_id or context['api_organization_id'],
-    solution_id = sol_id or context['api_solution_id']
-    response = oauth_request(
-        f"{context['api_url']}/organizations/{organization_id}/solutions/{solution_id}"
-        f"/runtemplates/{run_template_id}/handlers/{handler_id}", azure_token)
-    if response is None:
+    org_id = context['api_organization_id']
+    sol_id = context['api_solution_id']
+    check = blob_client.get_container_client(container=org_id)
+    if not check.exists():
+        logger.info(f"Container '{org_id}' not found")
         return CommandResponse.fail()
-    logger.info("Successfully downloaded solution handler")
+    client = blob_client.get_blob_client(container=org_id,
+                                         blob=f"{sol_id}/{run_template_id}/{handler_id}.zip")
+    data = client.download_blob().readall()
+    zf = zipfile.ZipFile("data.zip", mode='w', compression=zipfile.ZIP_DEFLATED)
+    zf.writestr(basename(client.blob_name), data)
+    zf.extractall(".")
+    os.remove("data.zip")
+    logger.info("Successfully download handler file")
     return CommandResponse.success()
--- Babylon/commands/api/solutions/handler/upload.py
+++ Babylon/commands/api/solutions/handler/upload.py
@@ -1,16 +1,16 @@
 import pathlib
-from logging import getLogger
 
+from logging import getLogger
 from typing import Any
 from click import command
 from click import option
 from click import argument
 from click import Path
+from azure.storage.blob import BlobServiceClient
+from Babylon.utils.clients import pass_blob_client
 from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
 from Babylon.utils.decorators import timing_decorator
 from Babylon.utils.response import CommandResponse
-from Babylon.utils.credentials import pass_azure_token
-from Babylon.utils.request import oauth_request
 from Babylon.utils.typing import QueryType
 from Babylon.utils.environment import Environment
 
@@ -20,8 +20,8 @@ env = Environment()
 
 @command()
 @wrapcontext()
+@pass_blob_client
 @timing_decorator
-@pass_azure_token("csm_api")
 @option(
     "--run-template",
     "run_template_id",
@@ -32,28 +32,28 @@ env = Environment()
 @option("--override", "override", is_flag=True, help="Override handler solution")
 @argument("handler_id", type=QueryType())
 @argument("handler_path", type=Path(path_type=pathlib.Path, exists=True))
-@inject_context_with_resource({'api': ['url', 'organization_id', "solution_id"]})
+@inject_context_with_resource({'api': ['url', 'organization_id', 'solution_id']})
 def upload(context: Any,
-           azure_token: str,
+           blob_client: BlobServiceClient,
            handler_path: pathlib.Path,
            handler_id: str,
            run_template_id: str,
            override: bool = False) -> CommandResponse:
     """Upload a solution handler zip to the solution"""
-    org_api = context['api_organization_id']
+    org_id = context['api_organization_id']
+    sol_id = context['api_solution_id']
     if not handler_path.suffix == ".zip":
         logger.error("solution handler upload only supports zip files")
         return CommandResponse.fail()
-    with open(handler_path, "rb") as handler:
-        response = oauth_request(
-            f"{context['api_url']}/organizations/{org_api}/solutions/{context['api_solution_id']}"
-            f"/runtemplates/{run_template_id}/handlers/{handler_id}",
-            azure_token,
-            files={handler_path.name: handler},
-            params={"overwrite": override},
-            headers={"Content-Type": "application/octet-stream"},
-            type="POST")
-    if response is None:
+    check = blob_client.get_container_client(container=org_id)
+    if not check.exists():
+        logger.info(f"Container '{org_id}' not found")
         return CommandResponse.fail()
-    logger.info(f"Successfully sent handler file {handler_path} to solution {context['api_solution_id']}")
+    client = blob_client.get_blob_client(container=org_id,
+                                         blob=f"{sol_id}/{run_template_id}/{handler_id}.zip")
+    if override and client.exists():
+        client.delete_blob()
+    with open(handler_path, "rb") as data:
+        client.upload_blob(data)
+    logger.info(f"Successfully sent handler file to solution {context['api_solution_id']}")
     return CommandResponse.success()
