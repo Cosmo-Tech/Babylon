40b0386eb2136945b0c498a81563a70f6dcf6520
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-01 20:51:43 +0100

    add service azure acr

--- Babylon/commands/azure/acr/delete.py
+++ Babylon/commands/azure/acr/delete.py
@@ -1,17 +1,14 @@
 import logging
 
-from typing import Any, Optional
-from azure.core.exceptions import HttpResponseError
-from azure.core.exceptions import ResourceNotFoundError
-from click import command
 from click import option
-from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
-from Babylon.utils.decorators import timing_decorator
-from Babylon.utils.interactive import confirm_deletion
-from Babylon.utils.response import CommandResponse
+from click import command
+from typing import Any, Optional
 from Babylon.utils.typing import QueryType
 from Babylon.utils.environment import Environment
-from Babylon.utils.clients import get_registry_client
+from Babylon.utils.response import CommandResponse
+from Babylon.utils.decorators import timing_decorator
+from Babylon.commands.azure.acr.service.api import AzureContainerRegistryService
+from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
 
 logger = logging.getLogger("Babylon")
 env = Environment()
@@ -27,27 +24,11 @@ def delete(context: Any, image: Optional[str] = None, force_validation: Optional
     """
     Delete docker image from selected repository
     """
-    registry_server = context['acr_login_server']
-    cr_client = get_registry_client(registry_server)
-    image = image or f"{context['acr_simulator_repository']}:{context['acr_simulator_version']}"
-    if not image:
-        logger.info(f"You trying to use the image in {env.context_id}.{env.environ_id}.acr.yaml")
-        logger.info(f"Current value: {context['acr_simulator_repository']}:{context['acr_simulator_version']}")
-    if not force_validation and not confirm_deletion("image", image):
-        return CommandResponse.fail()
-
-    image = f"{image}:latest" if ":" not in image else image
-    try:
-        props = cr_client.get_manifest_properties(*image.split(":"))
-    except ResourceNotFoundError:
-        logger.error(f"Image {image} not found in registry {registry_server}")
-        return CommandResponse.fail()
-
-    logger.info(f"Deleting image {image} from registry {registry_server}")
-    try:
-        cr_client.delete_manifest(props.repository_name, props.digest)
-    except HttpResponseError as e:
-        logger.error(f"Could not delete image {image} from registry {registry_server}: {str(e)}")
-        return CommandResponse.fail()
-    logger.info(f"Successfully deleted image {image} from registry {registry_server}")
+    acrApi = AzureContainerRegistryService()
+    state = dict()
+    state['acr'] = dict()
+    state['acr']['login_server'] = context['acr_login_server']
+    state['acr']['simulator_repository'] = context['acr_simulator_repository']
+    state['acr']['simulator_version'] = context['acr_simulator_version']
+    acrApi.delete(state, image_tag=image)
     return CommandResponse.success()
