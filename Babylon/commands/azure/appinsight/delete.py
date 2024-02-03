f5ffa9f23ffd630dc7b9a59d094413a4a5d1607e
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-03 15:23:11 +0100

    add service appinsight

--- Babylon/commands/azure/appinsight/delete.py
+++ Babylon/commands/azure/appinsight/delete.py
@@ -2,11 +2,10 @@ import logging
 
 from typing import Any
 from click import command, argument, option
+from Babylon.commands.azure.appinsight.service.api import AzureAppInsightService
 from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
 from Babylon.utils.response import CommandResponse
-from Babylon.utils.request import oauth_request
 from Babylon.utils.credentials import pass_azure_token
-from Babylon.utils.interactive import confirm_deletion
 from Babylon.utils.environment import Environment
 from Babylon.utils.typing import QueryType
 
@@ -19,23 +18,19 @@ env = Environment()
 @pass_azure_token()
 @option("-D", "force_validation", is_flag=True, help="Force Delete")
 @argument("name", type=QueryType())
-@inject_context_with_resource({'azure': ['resource_group_name', 'subscription_id']})
-def delete(context: Any, azure_token: str, name: str, force_validation: str) -> CommandResponse:
+@inject_context_with_resource({"azure": ["resource_group_name", "subscription_id"]})
+def delete(
+    context: Any, azure_token: str, name: str, force_validation: str
+) -> CommandResponse:
     """
     Delete app insight data from a name
     https://learn.microsoft.com/en-us/rest/api/application-insights/components/delete
     """
-    azure_subscription = context['azure_subscription_id']
-    resource_group_name = context['azure_resource_group_name']
-    if not force_validation and not confirm_deletion("appinsight", name):
-        return CommandResponse.fail()
-    route = (f'https://management.azure.com/subscriptions/{azure_subscription}/resourceGroups/{resource_group_name}/'
-             f'providers/Microsoft.Insights/components/{name}?api-version=2015-05-01')
-    response = oauth_request(route, azure_token, type="DELETE")
-    if response is None:
-        return CommandResponse.fail()
-    if response.status_code == 204:
-        logger.warn("App Insight not found")
-        return CommandResponse.fail()
-    logger.info("Successfully deleted")
+    apiAppInsight = AzureAppInsightService()
+    apiAppInsight.delete(
+        name=name,
+        context=context,
+        force_validation=force_validation,
+        azure_token=azure_token,
+    )
     return CommandResponse.success()
