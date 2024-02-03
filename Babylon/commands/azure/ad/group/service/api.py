e0e35cd3a0361444720b6fcafff98ada912fa902
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-03 16:48:39 +0100

    refacto ad app group svc

--- Babylon/commands/azure/ad/group/service/api.py
+++ Babylon/commands/azure/ad/group/service/api.py
@@ -1,13 +1,10 @@
-
 import jmespath
+
 from Babylon.utils.request import oauth_request
 from Babylon.utils.response import CommandResponse
 
 
 class AzureDirectoyGroupService:
-
-    def __init__(self) -> None:
-        pass
 
     def get_all(self, azure_token: str):
         route = "https://graph.microsoft.com/v1.0/groups/"
