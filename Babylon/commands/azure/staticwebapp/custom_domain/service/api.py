c5c835f4d224ae21afeda84e27a1cb219d1cdcf3
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-03 17:19:40 +0100

    refacto swa svc

--- Babylon/commands/azure/staticwebapp/custom_domain/service/api.py
+++ Babylon/commands/azure/staticwebapp/custom_domain/service/api.py
@@ -12,18 +12,20 @@ env = Environment()
 
 class AzureSWACustomDomainService:
 
+    def __init__(self, azure_token: str, state: dict = None) -> None:
+        self.state = state
+        self.azure_token = azure_token
+
     def upsert(
         self,
         webapp_name: str,
         domain_name: str,
-        context: dict,
         create_file: Path,
-        azure_token: str,
     ):
         check_ascii(webapp_name)
         check_ascii(domain_name)
-        azure_subscription = context["azure_subscription_id"]
-        resource_group_name = context["azure_resource_group_name"]
+        azure_subscription = self.state["azure"]["subscription_id"]
+        resource_group_name = self.state["azure"]["resource_group_name"]
         if not domain_name:
             return CommandResponse.fail()
         create_route = (
@@ -33,33 +35,33 @@ class AzureSWACustomDomainService:
         details = '{"properties":{}}'
         if create_file:
             details = env.fill_template(create_file)
-        response = oauth_request(create_route, azure_token, type="PUT", data=details)
+        response = oauth_request(create_route, self.azure_token, type="PUT", data=details)
         if response is None:
             return CommandResponse.fail()
         output_data = response.json()
         logger.info("Successfully launched")
         return output_data
 
-    def get_all(self, webapp_name: str, context: dict, azure_token: str):
-        azure_subscription = context['azure_subscription_id']
-        resource_group_name = context['azure_resource_group_name']
+    def get_all(self, webapp_name: str):
+        azure_subscription = self.state["azure"]["subscription_id"]
+        resource_group_name = self.state["azure"]["resource_group_name"]
         response = oauth_request(
             f"https://management.azure.com/subscriptions/{azure_subscription}/resourceGroups/{resource_group_name}/"
             f"providers/Microsoft.Web/staticSites/{webapp_name}/customDomains?api-version=2022-03-01",
-            azure_token,
+            self.azure_token,
             type="GET")
         if response is None:
             return CommandResponse.fail()
         output_data = response.json().get("value")
         return output_data
 
-    def get(self, webapp_name: str, domain_name: str, context: dict, azure_token: str):
-        azure_subscription = context['azure_subscription_id']
-        resource_group_name = context['azure_resource_group_name']
+    def get(self, webapp_name: str, domain_name: str):
+        azure_subscription = self.state["azure"]["subscription_id"]
+        resource_group_name = self.state["azure"]["resource_group_name"]
         response = oauth_request(
             f"https://management.azure.com/subscriptions/{azure_subscription}/resourceGroups/{resource_group_name}/"
             f"providers/Microsoft.Web/staticSites/{webapp_name}/customDomains/{domain_name}?api-version=2022-03-01",
-            azure_token,
+            self.azure_token,
             type="GET")
         if response is None:
             return CommandResponse.fail()
@@ -71,11 +73,9 @@ class AzureSWACustomDomainService:
         webapp_name: str,
         domain_name: str,
         force_validation: bool,
-        context: dict,
-        azure_token: str,
     ):
-        azure_subscription = context["azure_subscription_id"]
-        resource_group_name = context["azure_resource_group_name"]
+        azure_subscription = self.state["azure"]["subscription_id"]
+        resource_group_name = self.state["azure"]["resource_group_name"]
         if not force_validation and not confirm_deletion("domain", domain_name):
             return CommandResponse.fail()
         logger.info(
@@ -84,7 +84,7 @@ class AzureSWACustomDomainService:
         response = oauth_request(
             f"https://management.azure.com/subscriptions/{azure_subscription}/resourceGroups/{resource_group_name}/"
             f"providers/Microsoft.Web/staticSites/{webapp_name}/customDomains/{domain_name}?api-version=2022-03-01",
-            azure_token,
+            self.azure_token,
             type="DELETE",
         )
         if response is None:
