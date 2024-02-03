96e3dd44dbe21028c00fa10fe9bdc4d75b2c1ccd
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-03 15:43:37 +0100

    add app settings service

--- Babylon/commands/azure/staticwebapp/app_settings/get.py
+++ Babylon/commands/azure/staticwebapp/app_settings/get.py
@@ -3,7 +3,7 @@ import logging
 from typing import Any
 from click import command
 from click import argument
-from Babylon.utils.request import oauth_request
+from Babylon.commands.azure.staticwebapp.app_settings.service.api import AzureSWASettingsAppService
 from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
 from Babylon.utils.response import CommandResponse
 from Babylon.utils.credentials import pass_azure_token
@@ -24,14 +24,10 @@ def get(context: Any, azure_token: str, webapp_name: str) -> CommandResponse:
     Get static webapp app settings for the given static web app
     https://learn.microsoft.com/en-us/rest/api/appservice/static-sites/list-static-site-app-settings
     """
-    azure_subscription = context['azure_subscription_id']
-    resource_group_name = context['azure_resource_group_name']
-    response = oauth_request(
-        f"https://management.azure.com/subscriptions/{azure_subscription}/resourceGroups/{resource_group_name}/"
-        f"providers/Microsoft.Web/staticSites/{webapp_name}/listAppSettings?api-version=2022-03-01",
-        azure_token,
-        type="POST")
-    if response is None:
-        return CommandResponse.fail()
-    output_data = response.json()
-    return CommandResponse.success(output_data, verbose=True)
+    api_swa_settings = AzureSWASettingsAppService()
+    response = api_swa_settings.update(
+        webapp_name=webapp_name,
+        context=context,
+        azure_token=azure_token,
+    )
+    return CommandResponse.success(response, verbose=True)
