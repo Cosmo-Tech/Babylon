49dd8dddd45c55feb15e9f55e05f38f36db714af
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-03 15:35:40 +0100

    add iam service

--- Babylon/commands/azure/permission/set.py
+++ Babylon/commands/azure/permission/set.py
@@ -1,17 +1,17 @@
 import logging
-import uuid
 
 from typing import Any, Optional
 from azure.mgmt.authorization import AuthorizationManagementClient
-from azure.mgmt.authorization.models import RoleAssignmentCreateParameters
 from click import Choice, option
 from click import command
+from Babylon.commands.azure.permission.service.api import AzureIamService
 from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
 from Babylon.utils.response import CommandResponse
 from Babylon.utils.environment import Environment
 from Babylon.utils.typing import QueryType
 from Babylon.utils.clients import (
-    pass_iam_client, )
+    pass_iam_client,
+)
 
 logger = logging.getLogger("Babylon")
 env = Environment()
@@ -20,20 +20,36 @@ env = Environment()
 @command()
 @wrapcontext()
 @pass_iam_client
-@option("--resource-type", "resource_type", type=QueryType(), help="Ressource Type Id Azure")
-@option("--resource-name", "resource_name", type=QueryType(), help="Ressource Name Azure")
-@option("--principal-type",
-        "principal_type",
-        type=Choice(["User", "Group", "ServicePrincipal", "ForeignGroup", "Device"]),
-        default="ServicePrincipal",
-        help="Principal Type Azure")
-@option("--principal-id", "principal_id", type=QueryType(), required=True, help="Principal Id Ressource")
-@option("--role-id", "role_id", type=QueryType(), required=True, help="Role Id Ressource")
-@inject_context_with_resource({
-    'api': ['organization_id', 'workspace_key'],
-    'azure': ['resource_group_name', 'subscription_id'],
-    'platform': ['principal_id']
-})
+@option(
+    "--resource-type", "resource_type", type=QueryType(), help="Ressource Type Id Azure"
+)
+@option(
+    "--resource-name", "resource_name", type=QueryType(), help="Ressource Name Azure"
+)
+@option(
+    "--principal-type",
+    "principal_type",
+    type=Choice(["User", "Group", "ServicePrincipal", "ForeignGroup", "Device"]),
+    default="ServicePrincipal",
+    help="Principal Type Azure",
+)
+@option(
+    "--principal-id",
+    "principal_id",
+    type=QueryType(),
+    required=True,
+    help="Principal Id Ressource",
+)
+@option(
+    "--role-id", "role_id", type=QueryType(), required=True, help="Role Id Ressource"
+)
+@inject_context_with_resource(
+    {
+        "api": ["organization_id", "workspace_key"],
+        "azure": ["resource_group_name", "subscription_id"],
+        "platform": ["principal_id"],
+    }
+)
 def set(
     context: Any,
     iam_client: AuthorizationManagementClient,
@@ -46,22 +62,14 @@ def set(
     """
     Assign a new role in resource given
     """
-    organization_id = context['api_organization_id']
-    workspace_key = context['api_workspace_key']
-    azure_subscription = context['azure_subscription_id']
-    resource_group_name = context['azure_resource_group_name']
-    principal_id = principal_id or context['platform_principal_id']
-
-    resource_name = resource_name or f"{organization_id.lower()}-{workspace_key.lower()}"
-    prefix = f"/subscriptions/{azure_subscription}"
-    scope = f"{prefix}/resourceGroups/{resource_group_name}/providers/{resource_type}/{resource_name}"
-    role = f"{prefix}/providers/Microsoft.Authorization/roleDefinitions/{role_id}"
-    try:
-        iam_client.role_assignments.create(scope=scope,
-                                           role_assignment_name=str(uuid.uuid4()),
-                                           parameters=RoleAssignmentCreateParameters(role_definition_id=role,
-                                                                                     principal_id=principal_id,
-                                                                                     principal_type=principal_type))
-    except Exception as e:
-        logger.error(f"Failed to assign a new role: {e}")
+    apiIam = AzureIamService()
+    apiIam.set(
+        context=context,
+        principal_id=principal_id,
+        principal_type=principal_type,
+        resource_type=resource_type,
+        resource_name=resource_name,
+        role_id=role_id,
+        iam_client=iam_client
+    )
     return CommandResponse.success()
