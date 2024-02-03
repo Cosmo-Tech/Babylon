8118d5aeaabe93a59840870cca641d049d91591a
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-03 17:16:22 +0100

    refacto iam svc

--- Babylon/commands/azure/permission/service/api.py
+++ Babylon/commands/azure/permission/service/api.py
@@ -9,21 +9,25 @@ logger = logging.getLogger("Babylon")
 
 class AzureIamService:
 
+    def __init__(
+        self, iam_client: AuthorizationManagementClient, state: dict = None
+    ) -> None:
+        self.state = state
+        self.iam_client = iam_client
+
     def set(
         self,
-        context: dict,
         principal_id: str,
         principal_type: str,
         resource_name: str,
         resource_type: str,
         role_id: str,
-        iam_client: AuthorizationManagementClient,
     ):
-        organization_id = context["api_organization_id"]
-        workspace_key = context["api_workspace_key"]
-        azure_subscription = context["azure_subscription_id"]
-        resource_group_name = context["azure_resource_group_name"]
-        principal_id = principal_id or context["platform_principal_id"]
+        organization_id = self.state["api"]["organization_id"]
+        workspace_key = self.state["api"]["workspace_key"]
+        azure_subscription = self.state["azure"]["subscription_id"]
+        resource_group_name = self.state["azure"]["resource_group_name"]
+        principal_id = principal_id or self.state["platform"]["principal_id"]
 
         resource_name = (
             resource_name or f"{organization_id.lower()}-{workspace_key.lower()}"
@@ -32,7 +36,7 @@ class AzureIamService:
         scope = f"{prefix}/resourceGroups/{resource_group_name}/providers/{resource_type}/{resource_name}"
         role = f"{prefix}/providers/Microsoft.Authorization/roleDefinitions/{role_id}"
         try:
-            iam_client.role_assignments.create(
+            self.iam_client.role_assignments.create(
                 scope=scope,
                 role_assignment_name=str(uuid.uuid4()),
                 parameters=RoleAssignmentCreateParameters(
