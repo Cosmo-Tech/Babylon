bc2976d721ed1ba8c4a12516b87b1a298d15b536
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-03 16:39:52 +0100

    add user token svc

--- Babylon/commands/azure/token/get.py
+++ Babylon/commands/azure/token/get.py
@@ -1,6 +1,7 @@
 import logging
 
 from click import Choice, command, option
+from Babylon.commands.azure.token.service.api import AzureTokenService
 from Babylon.utils.decorators import wrapcontext
 from Babylon.utils.environment import Environment
 from Babylon.utils.response import CommandResponse
@@ -12,13 +13,17 @@ env = Environment()
 @command()
 @wrapcontext()
 @option("--email", "email", help="User email")
-@option("--scope", "scope", type=Choice(['default', "powerbi", "graph"]), required=True, help="API Scope")
+@option(
+    "--scope",
+    "scope",
+    type=Choice(["default", "powerbi", "graph"]),
+    required=True,
+    help="API Scope",
+)
 def get(scope: str, email: str) -> CommandResponse:
     """
     Get un acces token using a secret key
     """
-    email = email or env.configuration.get_var(resource_id="azure", var_name="email")
-    access_token = env.get_access_token_with_refresh_token(username=email, internal_scope=scope)
-    if access_token:
-        print(access_token)
-    return CommandResponse.success()
+    api_token = AzureTokenService()
+    response = api_token.get(email=email, scope=scope)
+    return CommandResponse.success(response, verboser=True)
