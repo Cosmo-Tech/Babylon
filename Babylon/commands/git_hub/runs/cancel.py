dc52092592fb133cd86e1b57363384840d0abd99
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-03 17:40:28 +0100

    add github svc

--- Babylon/commands/git_hub/runs/cancel.py
+++ Babylon/commands/git_hub/runs/cancel.py
@@ -1,8 +1,8 @@
 import logging
-import requests
 
 from click import command
 from typing import Any, Optional
+from Babylon.commands.git_hub.runs.service.api import GitHubRunsService
 from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
 from Babylon.utils.environment import Environment
 from Babylon.utils.response import CommandResponse
@@ -18,16 +18,6 @@ def cancel(context: Any, workflow_name: Optional[str] = None) -> CommandResponse
     """
     Cancel a github workflow from run url
     """
-    github_secret = env.get_global_secret(resource="github", name="token")
-    run_url = context['github_run_url']
-    url = f"{run_url}/cancel"
-    response = requests.post(url,
-                             headers={
-                                 "Accept": "application/vnd.github+json",
-                                 "X-GitHub-Api-Version": "2022-11-28",
-                                 "Authorization": f"Bearer {github_secret}"
-                             })
-    if response is None:
-        return CommandResponse.fail()
-    response = response.json()
+    api_runs_github = GitHubRunsService(state=context)
+    response = api_runs_github.cancel()
     return CommandResponse.success(response, verbose=True)
--- Babylon/commands/git_hub/runs/get.py
+++ Babylon/commands/git_hub/runs/get.py
@@ -1,11 +1,10 @@
 import logging
-import requests
 
 from typing import Any, Optional
 from click import argument, command
+from Babylon.commands.git_hub.runs.service.api import GitHubRunsService
 from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
 from Babylon.utils.environment import Environment
-from Babylon.utils.messages import SUCCESS_CONFIG_UPDATED
 from Babylon.utils.response import CommandResponse
 from Babylon.utils.typing import QueryType
 
@@ -21,26 +20,6 @@ def get(context: Any, workflow_name: Optional[str] = None) -> CommandResponse:
     """
     Get github workflow 
     """
-    github_secret = env.get_global_secret(resource="github", name="token")
-    org = context["github_organization"] or env.configuration.get_var(resource_id="github", var_name="organization")
-    repo = context["github_repository"] or env.configuration.get_var(resource_id="github", var_name="repository")
-    url = f"https://api.github.com/repos/{org}/{repo}/actions/runs"
-    response = requests.get(url,
-                            headers={
-                                "Accept": "application/vnd.github+json",
-                                "X-GitHub-Api-Version": "2022-11-28",
-                                "Authorization": f"Bearer {github_secret}"
-                            })
-    if response is None:
-        return CommandResponse.fail()
-    response = response.json()
-    responses = [{"path": i.get("path"), "url": i.get("url")} for i in response['workflow_runs']]
-    workflow_name = f"azure-static-web-apps-{workflow_name}.yml"
-    for workflow in responses:
-        if workflow_name in workflow.get("path"):
-            run_url = workflow.get("url")
-            env.configuration.set_var(resource_id="github", var_name="run_url", var_value=run_url)
-            logger.info(SUCCESS_CONFIG_UPDATED("github", "run_url"))
-            env.configuration.set_var(resource_id="github", var_name="workflow_path", var_value=workflow.get("path"))
-            logger.info(SUCCESS_CONFIG_UPDATED("github", "workflow_path"))
-    return CommandResponse.success()
+    api_runs_github = GitHubRunsService(state=context)
+    response = api_runs_github.get(workflow_name=workflow_name)
+    return CommandResponse.success(response, verbose=True)
