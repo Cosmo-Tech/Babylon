db5d1307086cfda6303877db44afaae8ec2f67cb
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-03 15:23:11 +0100

    add service appinsight

--- Babylon/commands/azure/appinsight/get.py
+++ Babylon/commands/azure/appinsight/get.py
@@ -2,8 +2,8 @@ import logging
 
 from typing import Any
 from click import command, argument
+from Babylon.commands.azure.appinsight.service.api import AzureAppInsightService
 from Babylon.utils.response import CommandResponse
-from Babylon.utils.request import oauth_request
 from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
 from Babylon.utils.credentials import pass_azure_token
 from Babylon.utils.environment import Environment
@@ -17,18 +17,12 @@ env = Environment()
 @wrapcontext()
 @pass_azure_token()
 @argument("name", type=QueryType())
-@inject_context_with_resource({'azure': ['resource_group_name', 'subscription_id']})
+@inject_context_with_resource({"azure": ["resource_group_name", "subscription_id"]})
 def get(context: Any, azure_token: str, name: str) -> CommandResponse:
     """
     Get app insight data from a name
     https://learn.microsoft.com/en-us/rest/api/application-insights/components/get
     """
-    azure_subscription = context['azure_subscription_id']
-    resource_group_name = context['azure_resource_group_name']
-    route = (f'https://management.azure.com/subscriptions/{azure_subscription}/resourceGroups/{resource_group_name}/'
-             f'providers/Microsoft.Insights/components/{name}?api-version=2015-05-01')
-    response = oauth_request(route, azure_token)
-    if response is None:
-        return CommandResponse.fail()
-    output_data = response.json()
-    return CommandResponse.success(output_data, verbose=True)
+    apiAppInsight = AzureAppInsightService()
+    response = apiAppInsight.get(name=name, context=context, azure_token=azure_token)
+    return CommandResponse.success(response, verbose=True)
--- Babylon/commands/azure/appinsight/get_all.py
+++ Babylon/commands/azure/appinsight/get_all.py
@@ -1,10 +1,9 @@
 import logging
-import jmespath
 
 from typing import Any, Optional
 from click import command, option
+from Babylon.commands.azure.appinsight.service.api import AzureAppInsightService
 from Babylon.utils.response import CommandResponse
-from Babylon.utils.request import oauth_request
 from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
 from Babylon.utils.credentials import pass_azure_token
 from Babylon.utils.environment import Environment
@@ -19,20 +18,14 @@ env = Environment()
 @output_to_file
 @pass_azure_token()
 @option("--filter", "filter", help="Filter response with a jmespath query")
-@inject_context_with_resource({'azure': ['resource_group_name', 'subscription_id']})
-def get_all(context: Any, azure_token: str, filter: Optional[str] = None) -> CommandResponse:
+@inject_context_with_resource({"azure": ["resource_group_name", "subscription_id"]})
+def get_all(
+    context: Any, azure_token: str, filter: Optional[str] = None
+) -> CommandResponse:
     """
     get all app insight data from a resource group
     https://learn.microsoft.com/en-us/rest/api/application-insights/components/list-by-resource-group
     """
-    azure_subscription = context['azure_subscription_id']
-    resource_group_name = context['azure_resource_group_name']
-    route = (f'https://management.azure.com/subscriptions/{azure_subscription}/resourceGroups/{resource_group_name}/'
-             f'providers/Microsoft.Insights/components?api-version=2015-05-01')
-    response = oauth_request(route, azure_token)
-    if response is None:
-        return CommandResponse.fail()
-    output_data = response.json()["value"]
-    if filter:
-        output_data = jmespath.search(filter, output_data)
-    return CommandResponse.success(output_data, verbose=True)
+    apiAppInsight = AzureAppInsightService()
+    response = apiAppInsight.get_all(context=context, azure_token=azure_token)
+    return CommandResponse.success(response, verbose=True)
