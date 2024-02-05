091888d4c2064921ce6e57600f1cc49f28052f15
Leopold Cramer <leopold.h.cramer@gmail.com>
2024-02-05 17:38:32 +0100

    cleanup commands

--- Babylon/commands/api/scenarioruns/__init__.py
+++ Babylon/commands/api/scenarioruns/__init__.py
@@ -1,6 +1,6 @@
 from click import group
 
-from Babylon.commands.api.scenarioruns.cumulatedlogs import cumulatedlogs
+from Babylon.commands.api.scenarioruns.cumulated_logs import cumulated_logs
 from Babylon.commands.api.scenarioruns.logs import logs
 from Babylon.commands.api.scenarioruns.status import status
 from Babylon.commands.api.scenarioruns.stop import stop
@@ -12,7 +12,7 @@ def scenarioruns():
     pass
 
 
-list_commands = [cumulatedlogs, logs, status, stop]
+list_commands = [cumulated_logs, logs, status, stop]
 
 for _command in list_commands:
     scenarioruns.add_command(_command)
--- Babylon/commands/api/scenarioruns/service/api.py
+++ Babylon/commands/api/scenarioruns/service/api.py
@@ -1,28 +1,46 @@
+from typing import Optional
+
 from Babylon.utils.request import oauth_request
 
 
 class ScenarioRunService:
-    def __init__(self, state: dict, spec: dict, azure_token: str):
+    def __init__(self, state: dict, azure_token: str, spec: Optional[dict] = None):
         self.state = state
         self.spec = spec
         self.azure_token = azure_token
 
     def logs(self):
-        return oauth_request(
-            f"{self.state['api']['url']}/organizations/{self.state['api']['organization_id']}/scenarioruns/{self.state['api']['scenariorun_id']}/logs",
+        url = self.state["state"]["api"]["url"]
+        organization_id = self.state["state"]["api"]["organization_id"]
+        scenariorun_id = self.state["state"]["api"]["scenariorun_id"]
+        response = oauth_request(
+            f"{url}/organizations/{organization_id}/scenarioruns/{scenariorun_id}/logs",
             self.azure_token)
+        return response
 
-    def cumulatedlogs(self):
-        return oauth_request(
-            f"{self.state['api']['url']}/organizations/{self.state['api']['organization_id']}/scenarioruns/{self.state['api']['scenariorun_id']}/cumulatedlogs",
+    def cumulated_logs(self):
+        url = self.state["state"]["api"]["url"]
+        organization_id = self.state["state"]["api"]["organization_id"]
+        scenariorun_id = self.state["state"]["api"]["scenariorun_id"]
+        response = oauth_request(
+            f"{url}/organizations/{organization_id}/scenarioruns/{scenariorun_id}/cumulatedlogs",
             self.azure_token)
+        return response
 
     def status(self):
-        return oauth_request(
-            f"{self.state['api']['url']}/organizations/{self.state['api']['organization_id']}/scenarioruns/{self.state['api']['scenariorun_id']}/status",
+        url = self.state["state"]["api"]["url"]
+        organization_id = self.state["state"]["api"]["organization_id"]
+        scenariorun_id = self.state["state"]["api"]["scenariorun_id"]
+        response = oauth_request(
+            f"{url}/organizations/{organization_id}/scenarioruns/{scenariorun_id}/status",
             self.azure_token)
+        return response
 
     def stop(self):
-        return oauth_request(
-            f"{self.state['api']['url']}/organizations/{self.state['api']['organization_id']}/scenarioruns/{self.state['api']['scenariorun_id']}/stop",
-            self.azure_token)
+        url = self.state["state"]["api"]["url"]
+        organization_id = self.state["state"]["api"]["organization_id"]
+        scenariorun_id = self.state["state"]["api"]["scenariorun_id"]
+        response = oauth_request(
+            f"{url}/organizations/{organization_id}/scenarioruns/{scenariorun_id}/stop",
+            self.azure_token, "POST")
+        return response
