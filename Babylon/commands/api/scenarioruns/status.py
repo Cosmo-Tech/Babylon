416d5a9a9cc512cc30c62daa72e03d231f81a1f1
Leopold Cramer <leopold.h.cramer@gmail.com>
2024-02-05 17:38:32 +0100

    cleanup commands

--- Babylon/commands/api/scenarioruns/status.py
+++ Babylon/commands/api/scenarioruns/status.py
@@ -1,44 +1,44 @@
+import json
 from logging import getLogger
 
-from click import command
+from click import command, option
 
 from Babylon.commands.api.scenarioruns.service.api import ScenarioRunService
-from Babylon.utils.decorators import timing_decorator
+from Babylon.utils.credentials import get_azure_token
+from Babylon.utils.decorators import timing_decorator, wrapcontext
 from Babylon.utils.response import CommandResponse
+from Babylon.utils.typing import QueryType
 
 logger = getLogger("Babylon")
+payload = json.dumps({
+    "organization_id": "",
+    "scenariorun_id": ""
+})
 
 
 @command()
-# @wrapcontext()
-# @pass_azure_token('csm_api')
-# @get-stuff-from-config
-# @option("--o", "organization_id", required=False)
-# @option("--o", "scenariorun_id", required=False)
+@wrapcontext()
 @timing_decorator
-def status(
-        # state: dict, spec: dict, azure_token: str, organization_id, scenariorun_id
-) -> CommandResponse:
+@option("--org-id", "organization_id", type=QueryType())
+@option("--scenariorun-id", "scenariorun_id", type=QueryType())
+def status(organization_id: str, scenariorun_id: str) -> CommandResponse:
     """
     Get the status of the scenarioRun
     """
-    # organization_id = organization_id or state['api']['organization_id']
+    token = get_azure_token("csm_api")
 
-    # scenariorun_id = scenariorun_id or state['api']['scenariorun_id']
+    state = {
+        "api_url": "https://dev.api.cosmotech.com",
+        "organization_id": "o-3z188zr63xk",
+        "scenariorun_id": ""
+    }
+    state['state']['api']['organization_id'] = organization_id or state['state']['api']['organization_id']
+    state['state']['api']['scenariorun_id'] = scenariorun_id or state['state']['api']['scenariorun_id']
 
-    logger.info(f"Getting status for scenariorun: 'scenariorun_id'")  # {context['scenariorun_id']}
-    service = ScenarioRunService(spec={}, state={
-        "api": {"url": "https://dev.api.cosmotech.com", "organization_id": "o-3z188zr63xk",
-                "scenariorun_id": "sr-7oq4p0znqy3"}},
-                                 azure_token=""
-                                 # state=state, spec=spec, azure_token=azure_token
-                                 )
-
+    logger.info(f"Getting status for scenariorun: {state['state']['api']['scenariorun_id']}")
+    service = ScenarioRunService(state=state, azure_token=token)
     response = service.status()
 
     if response is None:
         return CommandResponse.fail()
-
-    run_status = response.json()
-
-    return CommandResponse.success(run_status, verbose=True)
+    return CommandResponse.success(response.json(), verbose=True)
