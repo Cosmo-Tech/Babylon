52948e2c12a6d736731d744ad66fc050b3f95ed0
Leopold Cramer <leopold.h.cramer@gmail.com>
2024-02-05 10:19:20 +0100

    move files and add functionnement
    moved the scenariorun commands from abba file to api file
    added logic in commands

--- Babylon/commands/api/__init__.py
+++ Babylon/commands/api/__init__.py
@@ -3,6 +3,7 @@ from Babylon.utils.environment import Environment
 from .connectors import connectors
 from .datasets import datasets
 from .organizations import organizations
+from .scenarioruns import scenarioruns
 from .solutions import solutions
 from .workspaces import workspaces
 from .payloads import payloads
@@ -17,7 +18,7 @@ def api():
     env.check_environ(["BABYLON_SERVICE", "BABYLON_TOKEN", "BABYLON_ORG_NAME"])
 
 
-list_groups = [workspaces, datasets, connectors, organizations, solutions, payloads, scenarios]
+list_groups = [workspaces, datasets, connectors, organizations, solutions, payloads, scenarios, scenarioruns]
 
 for _group in list_groups:
     api.add_command(_group)
