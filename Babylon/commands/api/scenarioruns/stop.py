1b1761434af3689dffaa8c65f972cc8743624c40
Leopold Cramer <leopold.h.cramer@gmail.com>
2024-02-06 14:58:34 +0100

    integrate new state decorator

--- Babylon/commands/api/scenarioruns/stop.py
+++ Babylon/commands/api/scenarioruns/stop.py
@@ -1,43 +1,34 @@
-import json
 from logging import getLogger
+from typing import Any
 
 from click import command, option
 
 from Babylon.commands.api.scenarioruns.service.api import ScenarioRunService
-from Babylon.utils.credentials import get_azure_token
-from Babylon.utils.decorators import timing_decorator, wrapcontext
+from Babylon.utils.credentials import pass_azure_token
+from Babylon.utils.decorators import timing_decorator, wrapcontext, retrieve_state
 from Babylon.utils.response import CommandResponse
 from Babylon.utils.typing import QueryType
 
 logger = getLogger("Babylon")
-payload = json.dumps({
-    "organization_id": "",
-    "scenariorun_id": ""
-})
 
 
 @command()
 @wrapcontext()
 @timing_decorator
+@pass_azure_token("csm_api")
 @option("--org-id", "organization_id", type=QueryType())
 @option("--scenariorun-id", "scenariorun_id", type=QueryType())
-def stop(organization_id: str, scenariorun_id: str) -> CommandResponse:
+@retrieve_state
+def stop(state: Any, azure_token: str, organization_id: str, scenariorun_id: str) -> CommandResponse:
     """
     Stop the scenarioRun
     """
-    token = get_azure_token("csm_api")
+    state = state['state']
+    state['api']['organization_id'] = organization_id or state['api']['organization_id']
+    state['api']['scenariorun_id'] = scenariorun_id or state['api']['scenariorun_id']
 
-    state = {
-        "api_url": "https://dev.api.cosmotech.com",
-        "organization_id": "o-3z188zr63xk",
-        "scenariorun_id": ""
-    }
-
-    state['state']['api']['organization_id'] = organization_id or state['state']['api']['organization_id']
-    state['state']['api']['scenariorun_id'] = scenariorun_id or state['state']['api']['scenariorun_id']
-
-    logger.info(f"stopping scenariorun: {state['state']['api']['scenariorun_id']}")
-    service = ScenarioRunService(state=state, azure_token=token)
+    logger.info(f"stopping scenariorun: {state['api']['scenariorun_id']}")
+    service = ScenarioRunService(state=state, azure_token=azure_token)
     response = service.stop()
 
     if response is None:
