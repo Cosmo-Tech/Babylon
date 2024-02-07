9cd93d3754722292154c117d855494eaa396cbaa
Elena Sasova <sassova.elena@gmail.com>
2024-02-07 16:54:26 +0100

    fix: fixup

--- Babylon/commands/api/scenarios/get_all.py
+++ Babylon/commands/api/scenarios/get_all.py
@@ -1,31 +1,36 @@
-from click import command
+from click import command, option
+from typing import Any
 
 from Babylon.commands.api.scenarios.service.api import ScenarioService
 from Babylon.utils.credentials import pass_azure_token
-from Babylon.utils.decorators import timing_decorator, wrapcontext
+from Babylon.utils.decorators import timing_decorator, wrapcontext, retrieve_state
+from Babylon.utils.environment import Environment
 from Babylon.utils.response import CommandResponse
+
+env = Environment()
 
 
 @command()
 @wrapcontext()
 @pass_azure_token("csm_api")
 @timing_decorator
-def get_all(azure_token: str) -> CommandResponse:
+@option("--organization_id", "organization_id", type=str)
+@option("--workspace_id", "workspace_id", type=str)
+@retrieve_state
+def get_all(state: Any, organization_id: str, workspace_id: str, azure_token: str) -> CommandResponse:
     """
     Get all scenarios in the workspace
     """
-    state = {
-        "state": {
-            "api": {
-                "url": "https://dev.api.cosmotech.com",
-                "organization_id": "o-3z188zr63xk",
-                "workspace_id": "w-k91e49pgyw6",
-            }
-        },
-    }
-    scenario_service = ScenarioService(state=state, azure_token=azure_token)
-    scenario_service_response = scenario_service.get_all()
-    if scenario_service_response is None:
+
+    service_state = state["state"]
+    if organization_id:
+        service_state["api"]["organization_id"] = organization_id
+    if workspace_id:
+        service_state["api"]["workspace_id"] = workspace_id
+
+    scenario_service = ScenarioService(state=service_state, azure_token=azure_token)
+    response = scenario_service.get_all()
+    if response is None:
         return CommandResponse.fail()
-    response = scenario_service_response.json()
-    return CommandResponse.success(response, verbose=True)
+    scenarios = response.json()
+    return CommandResponse.success(scenarios, verbose=True)
