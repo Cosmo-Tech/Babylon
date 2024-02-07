f6fb9cf6b004bea3fb178d7d8484be8ad05b251b
Elena Sasova <sassova.elena@gmail.com>
2024-02-07 16:54:26 +0100

    fix: fixup

--- Babylon/commands/api/scenarios/create.py
+++ Babylon/commands/api/scenarios/create.py
@@ -1,11 +1,14 @@
-from click import command
+import json
+from typing import Any
+
+from click import command, option
 
 from Babylon.commands.api.scenarios.service.api import ScenarioService
 from Babylon.utils.credentials import pass_azure_token
-from Babylon.utils.decorators import timing_decorator, wrapcontext
+from Babylon.utils.decorators import timing_decorator, wrapcontext, retrieve_state
 from Babylon.utils.response import CommandResponse
 
-payload = {
+payload = json.dumps({
     "name": "Creating scenario with Babylon",
     "description": "Brewery master reference analysis",
     "tags": ["Brewery", "reference"],
@@ -17,32 +20,32 @@ payload = {
             "role": "admin"
         }],
     },
-}
+})
 
 
 @command()
 @wrapcontext()
+@retrieve_state
 @pass_azure_token("csm_api")
 @timing_decorator
-def create(azure_token: str) -> CommandResponse:
+@option("--organization_id", "organization_id", type=str)
+@option("--workspace_id", "workspace_id", type=str)
+def create(state: Any, organization_id: str, workspace_id: str, azure_token: str) -> CommandResponse:
     """
     Create new scenario
     """
+    service_state = state["state"]
+    if organization_id:
+        service_state["api"]["organization_id"] = organization_id
+    if workspace_id:
+        service_state["api"]["workspace_id"] = workspace_id
 
-    state = {
-        "state": {
-            "api": {
-                "url": "https://dev.api.cosmotech.com",
-                "organization_id": "o-3z188zr63xk",
-                "workspace_id": "w-k91e49pgyw6",
-            }
-        },
-    }
+    # change this line when scenario manipulation is specified
     spec = payload
 
-    scenario_service = ScenarioService(state=state, azure_token=azure_token, spec=spec)
-    scenario_service_response = scenario_service.create()
-    if scenario_service_response is None:
+    scenario_service = ScenarioService(state=service_state, azure_token=azure_token, spec=spec)
+    response = scenario_service.create()
+    if response is None:
         return CommandResponse.fail()
-    response = scenario_service_response.json()
-    return CommandResponse.success(response, verbose=True)
+    scenario = response.json()
+    return CommandResponse.success(scenario, verbose=True)
--- Babylon/commands/api/scenarios/delete.py
+++ Babylon/commands/api/scenarios/delete.py
@@ -1,10 +1,11 @@
 from logging import getLogger
+from typing import Any
 
-from click import command, argument
+from click import command, argument, option
 
 from Babylon.commands.api.scenarios.service.api import ScenarioService
 from Babylon.utils.credentials import pass_azure_token
-from Babylon.utils.decorators import timing_decorator, wrapcontext
+from Babylon.utils.decorators import timing_decorator, wrapcontext, retrieve_state
 from Babylon.utils.response import CommandResponse
 
 logger = getLogger("Babylon")
@@ -14,27 +15,34 @@ logger = getLogger("Babylon")
 @wrapcontext()
 @pass_azure_token("csm_api")
 @timing_decorator
+@retrieve_state
+@option("--organization_id", "organization_id", type=str)
+@option("--workspace_id", "workspace_id", type=str)
 @argument("scenario_id", required=False)
-def delete(azure_token: str, scenario_id: str) -> CommandResponse:
+def delete(
+    state: Any,
+    organization_id: str,
+    workspace_id: str,
+    azure_token: str,
+    scenario_id: str,
+) -> CommandResponse:
     """
     Delete a scenario
     """
     if not scenario_id:
         logger.error("Scenario id is missing")
         return CommandResponse.fail()
-    state = {
-        "state": {
-            "api": {
-                "url": "https://dev.api.cosmotech.com",
-                "organization_id": "o-3z188zr63xk",
-                "workspace_id": "w-k91e49pgyw6",
-                "scenario_id": scenario_id
-            }
-        },
-    }
-    scenario_service = ScenarioService(state=state, azure_token=azure_token)
-    scenario_service_response = scenario_service.delete()
-    if scenario_service_response is None:
+    service_state = state["state"]
+    if organization_id:
+        service_state["api"]["organization_id"] = organization_id
+    if workspace_id:
+        service_state["api"]["workspace_id"] = workspace_id
+
+    # change this line when scenario manipulation is specified
+    spec = {"scenario_id": scenario_id}
+
+    scenario_service = ScenarioService(state=service_state, azure_token=azure_token, spec=spec)
+    response = scenario_service.delete()
+    if response is None:
         return CommandResponse.fail()
-    response = scenario_service_response.json()
-    return CommandResponse.success(response, verbose=True)
+    logger.info("Scenario has been deleted")
