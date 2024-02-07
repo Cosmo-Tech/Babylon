cd3e08c5855090fbc489be57fbfeffc446d039be
Elena Sasova <sassova.elena@gmail.com>
2024-02-07 16:54:26 +0100

    fix: fixup

--- Babylon/commands/api/scenarios/update.py
+++ Babylon/commands/api/scenarios/update.py
@@ -1,40 +1,32 @@
+import json
 from logging import getLogger
+from typing import Any
 
-from click import command, argument
+from click import command, argument, option
 
 from Babylon.commands.api.scenarios.service.api import ScenarioService
 from Babylon.utils.credentials import pass_azure_token
-from Babylon.utils.decorators import timing_decorator, wrapcontext
+from Babylon.utils.decorators import timing_decorator, wrapcontext, retrieve_state
 from Babylon.utils.response import CommandResponse
 
-payload = {
-    "parametersValues": [
-        {
-            "parameterId": "stock",
-            "value": "6000",
-            "varType": "int"
-        },
-        {
-            "parameterId": "restock_qty",
-            "value": "25",
-            "varType": "int"
-        },
-        {
-            "parameterId": "nb_waiters",
-            "value": "5",
-            "varType": "int"
-        },
-    ]
-}
 logger = getLogger("Babylon")
 
 
 @command()
 @wrapcontext()
+@retrieve_state
 @pass_azure_token("csm_api")
 @timing_decorator
+@option("--organization_id", "organization_id", type=str)
+@option("--workspace_id", "workspace_id", type=str)
 @argument("scenario_id", required=False)
-def update(scenario_id: str, azure_token: str) -> CommandResponse:
+def update(
+    state: Any,
+    organization_id: str,
+    workspace_id: str,
+    scenario_id: str,
+    azure_token: str,
+) -> CommandResponse:
     """
     Update a scenario
     """
@@ -42,20 +34,38 @@ def update(scenario_id: str, azure_token: str) -> CommandResponse:
         logger.error("Scenario id is missing")
         return CommandResponse.fail()
 
-    state = {
-        "state": {
-            "api": {
-                "url": "https://dev.api.cosmotech.com",
-                "organization_id": "o-3z188zr63xk",
-                "workspace_id": "w-k91e49pgyw6",
-                "scenario_id": scenario_id
-            }
-        },
-    }
-    spec = payload
-    scenario_service = ScenarioService(state=state, spec=spec, azure_token=azure_token)
-    scenario_service_response = scenario_service.update()
-    if scenario_service_response is None:
+    service_state = state["state"]
+    if organization_id:
+        service_state["api"]["organization_id"] = organization_id
+    if workspace_id:
+        service_state["api"]["workspace_id"] = workspace_id
+
+    # change this line when scenario manipulation is specified
+    service_state["api"]["scenario_id"] = scenario_id
+
+    # change this line when scenario manipulation is specified
+    spec = json.dumps({
+        "parametersValues": [
+            {
+                "parameterId": "stock",
+                "value": "6000",
+                "varType": "int"
+            },
+            {
+                "parameterId": "restock_qty",
+                "value": "25",
+                "varType": "int"
+            },
+            {
+                "parameterId": "nb_waiters",
+                "value": "5",
+                "varType": "int"
+            },
+        ]
+    })
+    scenario_service = ScenarioService(state=service_state, spec=spec, azure_token=azure_token)
+    response = scenario_service.update()
+    if response is None:
         return CommandResponse.fail()
-    response = scenario_service_response.json()
-    return CommandResponse.success(response, verbose=True)
+    scenario = response.json()
+    return CommandResponse.success(scenario, verbose=True)
