0c51a3a97ca52e82cb89edd57f3d1d5652244f66
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-08 14:46:33 +0100

    add new state to arm

--- Babylon/commands/azure/arm/run.py
+++ Babylon/commands/azure/arm/run.py
@@ -13,7 +13,7 @@ from click import command
 from Babylon.utils.interactive import confirm_deploy_arm_mode
 from Babylon.utils.typing import QueryType
 from Babylon.utils.environment import Environment
-from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
+from Babylon.utils.decorators import retrieve_state, wrapcontext
 from Babylon.utils.response import CommandResponse
 from Babylon.utils.clients import pass_arm_client
 
@@ -27,9 +27,9 @@ env = Environment()
 @argument("deployment_name", type=QueryType())
 @option("--file", "deploy_file", type=str, help="Your custom arm description file yaml")
 @option("--complete-mode", "deploy_mode_complete", is_flag=True, help="Mode deployment")
-@inject_context_with_resource({'api': ['organization_id', 'workspace_key'], 'azure': ['resource_group_name']})
+@retrieve_state
 def run(
-    context: Any,
+    state: Any,
     arm_client: ResourceManagementClient,
     deployment_name: str,
     deploy_file: str,
@@ -38,9 +38,9 @@ def run(
     """
     Apply a resource deployment config via arm template file in working directory
     """
-    organization_id = context['api_organization_id']
-    workspace_key = context['api_workspace_key']
-    resource_group_name = context['azure_resource_group_name']
+    organization_id = state["api"]["organization_id"]
+    workspace_key = state["api"]["workspace_key"]
+    resource_group_name = state["azure"]["resource_group_name"]
 
     deploy_file = pathlib.Path(env.convert_template_path(deploy_file)) or pathlib.Path(deploy_file)
     mode = DeploymentMode.INCREMENTAL
