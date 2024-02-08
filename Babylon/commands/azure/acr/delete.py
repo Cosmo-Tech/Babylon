d3e3222210786065e92df50e35726d5457729a0a
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-08 14:25:25 +0100

    add new state in acr

--- Babylon/commands/azure/acr/delete.py
+++ Babylon/commands/azure/acr/delete.py
@@ -4,11 +4,11 @@ from click import option
 from click import command
 from typing import Any, Optional
 from Babylon.utils.typing import QueryType
+from Babylon.utils.decorators import wrapcontext
 from Babylon.utils.environment import Environment
 from Babylon.utils.response import CommandResponse
-from Babylon.utils.decorators import timing_decorator
+from Babylon.utils.decorators import retrieve_state, timing_decorator
 from Babylon.commands.azure.acr.service.api import AzureContainerRegistryService
-from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
 
 logger = logging.getLogger("Babylon")
 env = Environment()
@@ -18,17 +18,11 @@ env = Environment()
 @wrapcontext()
 @timing_decorator
 @option("--image", type=QueryType(), help="Remote docker image to pull, example hello-world:latest")
-@option("-D", "force_validation", is_flag=True, default=True, help="Force Delete")
-@inject_context_with_resource({'acr': ['login_server', 'simulator_repository', 'simulator_version']}, required=False)
-def delete(context: Any, image: Optional[str] = None, force_validation: Optional[bool] = False) -> CommandResponse:
+@retrieve_state
+def delete(state: Any, image: Optional[str] = None) -> CommandResponse:
     """
     Delete docker image from selected repository
     """
-    state = dict()
-    state['acr'] = dict()
-    state['acr']['login_server'] = context['acr_login_server']
-    state['acr']['simulator_repository'] = context['acr_simulator_repository']
-    state['acr']['simulator_version'] = context['acr_simulator_version']
     service = AzureContainerRegistryService(state=state)
     service.delete(state, image_tag=image)
     return CommandResponse.success()
--- Babylon/commands/azure/acr/pull.py
+++ Babylon/commands/azure/acr/pull.py
@@ -1,14 +1,14 @@
 import logging
 
-from typing import Any, Optional
-from click import command
 from click import option
-from Babylon.commands.azure.acr.service.api import AzureContainerRegistryService
-from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
-from Babylon.utils.decorators import timing_decorator
+from click import command
+from typing import Any, Optional
 from Babylon.utils.typing import QueryType
 from Babylon.utils.environment import Environment
 from Babylon.utils.response import CommandResponse
+from Babylon.utils.decorators import timing_decorator
+from Babylon.utils.decorators import retrieve_state, wrapcontext
+from Babylon.commands.azure.acr.service.api import AzureContainerRegistryService
 
 logger = logging.getLogger("Babylon")
 env = Environment()
@@ -18,16 +18,11 @@ env = Environment()
 @wrapcontext()
 @timing_decorator
 @option("--image", type=QueryType(), help="Remote docker image to pull, example hello-world:latest")
-@inject_context_with_resource({'acr': ['login_server', 'simulator_repository', 'simulator_version']}, required=False)
-def pull(context: Any, image: Optional[str] = None) -> CommandResponse:
+@retrieve_state
+def pull(state: Any, image: Optional[str] = None) -> CommandResponse:
     """
     Pulls a docker image from the ACR registry
     """
-    state = dict()
-    state['acr'] = dict()
-    state['acr']['login_server'] = context['acr_login_server']
-    state['acr']['simulator_repository'] = context['acr_simulator_repository']
-    state['acr']['simulator_version'] = context['acr_simulator_version']
     service = AzureContainerRegistryService(state=state)
     service.pull(image_tag=image)
     return CommandResponse.success()
