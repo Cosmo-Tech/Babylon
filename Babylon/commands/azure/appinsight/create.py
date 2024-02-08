2d845943316e4fb63bfd8ecb64ee5b8c34ea0b5d
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-08 14:44:14 +0100

    add new state to appinsight

--- Babylon/commands/azure/appinsight/create.py
+++ Babylon/commands/azure/appinsight/create.py
@@ -6,7 +6,7 @@ from click import command, argument, option, Path
 from Babylon.commands.azure.appinsight.service.api import AzureAppInsightService
 from Babylon.utils.environment import Environment
 from Babylon.utils.response import CommandResponse
-from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
+from Babylon.utils.decorators import retrieve_state, wrapcontext
 from Babylon.utils.credentials import pass_azure_token
 from Babylon.utils.typing import QueryType
 
@@ -24,16 +24,14 @@ env = Environment()
     help="Your custom appinsight description file yaml",
 )
 @argument("name", type=QueryType())
-@inject_context_with_resource(
-    {"azure": ["resource_group_name", "subscription_id"], "webapp": ["location"]}
-)
+@retrieve_state
 def create(
-    context: Any, azure_token: str, name: str, file: Optional[pathlib.Path] = None
+    state: Any, azure_token: str, name: str, file: Optional[pathlib.Path] = None
 ) -> CommandResponse:
     """
     Create a app insight resource in the given resource group
     https://learn.microsoft.com/en-us/rest/api/application-insights/components/create-or-update
     """
-    service = AzureAppInsightService(azure_token=azure_token, state=context)
+    service = AzureAppInsightService(azure_token=azure_token, state=state)
     output_data = service.create(name=name, file=file)
     return CommandResponse.success(output_data, verbose=True)
--- Babylon/commands/azure/appinsight/get_all.py
+++ Babylon/commands/azure/appinsight/get_all.py
@@ -4,7 +4,7 @@ from typing import Any, Optional
 from click import command, option
 from Babylon.commands.azure.appinsight.service.api import AzureAppInsightService
 from Babylon.utils.response import CommandResponse
-from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
+from Babylon.utils.decorators import retrieve_state, wrapcontext
 from Babylon.utils.credentials import pass_azure_token
 from Babylon.utils.environment import Environment
 from Babylon.utils.decorators import output_to_file
@@ -18,14 +18,14 @@ env = Environment()
 @output_to_file
 @pass_azure_token()
 @option("--filter", "filter", help="Filter response with a jmespath query")
-@inject_context_with_resource({"azure": ["resource_group_name", "subscription_id"]})
+@retrieve_state
 def get_all(
-    context: Any, azure_token: str, filter: Optional[str] = None
+    state: Any, azure_token: str, filter: Optional[str] = None
 ) -> CommandResponse:
     """
     get all app insight data from a resource group
     https://learn.microsoft.com/en-us/rest/api/application-insights/components/list-by-resource-group
     """
-    service = AzureAppInsightService(azure_token=azure_token, state=context)
+    service = AzureAppInsightService(azure_token=azure_token, state=state)
     response = service.get_all()
     return CommandResponse.success(response, verbose=True)
