404be8e57b52cf9a2d62c3192611d18eefebec9b
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-08 14:44:14 +0100

    add new state to appinsight

--- Babylon/commands/azure/appinsight/delete.py
+++ Babylon/commands/azure/appinsight/delete.py
@@ -3,7 +3,7 @@ import logging
 from typing import Any
 from click import command, argument, option
 from Babylon.commands.azure.appinsight.service.api import AzureAppInsightService
-from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
+from Babylon.utils.decorators import retrieve_state, wrapcontext
 from Babylon.utils.response import CommandResponse
 from Babylon.utils.credentials import pass_azure_token
 from Babylon.utils.environment import Environment
@@ -18,15 +18,15 @@ env = Environment()
 @pass_azure_token()
 @option("-D", "force_validation", is_flag=True, help="Force Delete")
 @argument("name", type=QueryType())
-@inject_context_with_resource({"azure": ["resource_group_name", "subscription_id"]})
+@retrieve_state
 def delete(
-    context: Any, azure_token: str, name: str, force_validation: str
+    state: Any, azure_token: str, name: str, force_validation: str
 ) -> CommandResponse:
     """
     Delete app insight data from a name
     https://learn.microsoft.com/en-us/rest/api/application-insights/components/delete
     """
-    service = AzureAppInsightService(azure_token=azure_token, state=context)
+    service = AzureAppInsightService(azure_token=azure_token, state=state)
     service.delete(
         name=name,
         force_validation=force_validation,
