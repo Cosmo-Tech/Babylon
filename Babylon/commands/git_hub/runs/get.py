c47bb45d4de45f9ff7a5daddd31d4224290418ab
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-08 14:56:54 +0100

    add new state to github

--- Babylon/commands/git_hub/runs/get.py
+++ Babylon/commands/git_hub/runs/get.py
@@ -3,7 +3,7 @@ import logging
 from typing import Any, Optional
 from click import argument, command
 from Babylon.commands.git_hub.runs.service.api import GitHubRunsService
-from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
+from Babylon.utils.decorators import retrieve_state, wrapcontext
 from Babylon.utils.environment import Environment
 from Babylon.utils.response import CommandResponse
 from Babylon.utils.typing import QueryType
@@ -15,11 +15,11 @@ env = Environment()
 @command()
 @wrapcontext()
 @argument("workflow_name", type=QueryType())
-@inject_context_with_resource({'github': ['organization', 'repository']})
-def get(context: Any, workflow_name: Optional[str] = None) -> CommandResponse:
+@retrieve_state
+def get(state: Any, workflow_name: Optional[str] = None) -> CommandResponse:
     """
     Get github workflow 
     """
-    service = GitHubRunsService(state=context)
+    service = GitHubRunsService(state=state)
     response = service.get(workflow_name=workflow_name)
     return CommandResponse.success(response, verbose=True)
