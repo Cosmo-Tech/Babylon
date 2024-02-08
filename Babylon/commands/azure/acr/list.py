384cde4c849c6446fcf91ae473102e9ad1f1fbce
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-08 14:25:25 +0100

    add new state in acr

--- Babylon/commands/azure/acr/list.py
+++ Babylon/commands/azure/acr/list.py
@@ -1,9 +1,9 @@
 from typing import Any, Optional
 from click import argument, command
 from Babylon.utils.typing import QueryType
+from Babylon.utils.decorators import wrapcontext
 from Babylon.utils.response import CommandResponse
-from Babylon.utils.decorators import timing_decorator
-from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
+from Babylon.utils.decorators import retrieve_state, timing_decorator
 from Babylon.commands.azure.acr.service.api import AzureContainerRegistryService
 
 
@@ -11,14 +11,11 @@ from Babylon.commands.azure.acr.service.api import AzureContainerRegistryService
 @wrapcontext()
 @timing_decorator
 @argument("server", type=QueryType(), required=False)
-@inject_context_with_resource({'acr': ['login_server']})
-def list(context: Any, server: Optional[str] = None) -> CommandResponse:
+@retrieve_state
+def list(state: Any, server: Optional[str] = None) -> CommandResponse:
     """
     List all docker images in the specified registry
     """
-    state = dict()
-    state['acr'] = dict()
-    state['acr']['login_server'] = context['acr_login_server']
     service = AzureContainerRegistryService(state=state)
-    service.list()
+    service.list(server=server)
     CommandResponse.success()
