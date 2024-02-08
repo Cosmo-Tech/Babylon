b11a3d3355eb0b49d36da0553644be12c429e3c6
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-08 14:56:54 +0100

    add new state to github

--- Babylon/commands/git_hub/runs/cancel.py
+++ Babylon/commands/git_hub/runs/cancel.py
@@ -5,7 +5,7 @@ from click import command
 from Babylon.utils.environment import Environment
 from Babylon.utils.response import CommandResponse
 from Babylon.commands.git_hub.runs.service.api import GitHubRunsService
-from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
+from Babylon.utils.decorators import retrieve_state, wrapcontext
 
 logger = logging.getLogger("Babylon")
 env = Environment()
@@ -13,11 +13,11 @@ env = Environment()
 
 @command()
 @wrapcontext()
-@inject_context_with_resource({'github': ['run_url']})
-def cancel(context: Any) -> CommandResponse:
+@retrieve_state
+def cancel(state: Any) -> CommandResponse:
     """
     Cancel a github workflow from run url
     """
-    service = GitHubRunsService(state=context)
+    service = GitHubRunsService(state=state)
     response = service.cancel()
     return CommandResponse.success(response, verbose=True)
