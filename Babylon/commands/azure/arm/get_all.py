bb0ac5f69f2cfcfef15c142c41e42358986ab6ec
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-08 14:46:33 +0100

    add new state to arm

--- Babylon/commands/azure/arm/get_all.py
+++ Babylon/commands/azure/arm/get_all.py
@@ -4,7 +4,7 @@ from typing import Any
 from azure.core.exceptions import HttpResponseError
 from azure.mgmt.resource import ResourceManagementClient
 from click import command
-from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
+from Babylon.utils.decorators import retrieve_state, wrapcontext
 from Babylon.utils.decorators import timing_decorator
 from Babylon.utils.response import CommandResponse
 from Babylon.utils.clients import pass_arm_client
@@ -18,15 +18,15 @@ env = Environment()
 @wrapcontext()
 @timing_decorator
 @pass_arm_client
-@inject_context_with_resource({'azure': ['resource_group_name']})
+@retrieve_state
 def get_all(
-    context: Any,
+    state: Any,
     arm_client: ResourceManagementClient,
 ) -> CommandResponse:
     """
     Get all the deployments for a resource group
     """
-    resource_group_name = context['azure_resource_group_name']
+    resource_group_name = state["azure"]["resource_group_name"]
     try:
         deployment_list = arm_client.deployments.list_by_resource_group(resource_group_name)
     except HttpResponseError as _e:
