88d185993ac62f2476c0b51826249438362f0faa
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-08 14:44:14 +0100

    add new state to appinsight

--- Babylon/commands/azure/appinsight/get.py
+++ Babylon/commands/azure/appinsight/get.py
@@ -4,7 +4,7 @@ from typing import Any
 from click import command, argument
 from Babylon.commands.azure.appinsight.service.api import AzureAppInsightService
 from Babylon.utils.response import CommandResponse
-from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
+from Babylon.utils.decorators import retrieve_state, wrapcontext
 from Babylon.utils.credentials import pass_azure_token
 from Babylon.utils.environment import Environment
 from Babylon.utils.typing import QueryType
@@ -17,12 +17,12 @@ env = Environment()
 @wrapcontext()
 @pass_azure_token()
 @argument("name", type=QueryType())
-@inject_context_with_resource({"azure": ["resource_group_name", "subscription_id"]})
-def get(context: Any, azure_token: str, name: str) -> CommandResponse:
+@retrieve_state
+def get(state: Any, azure_token: str, name: str) -> CommandResponse:
     """
     Get app insight data from a name
     https://learn.microsoft.com/en-us/rest/api/application-insights/components/get
     """
-    service = AzureAppInsightService(azure_token=azure_token, state=context)
+    service = AzureAppInsightService(azure_token=azure_token, state=state)
     response = service.get(name=name)
     return CommandResponse.success(response, verbose=True)
