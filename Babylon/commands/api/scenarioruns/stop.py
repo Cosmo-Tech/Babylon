e780c0e8fa33438c7bfff6c7f2583040a8ace15f
Leopold Cramer <leopold.h.cramer@gmail.com>
2024-02-09 16:06:03 +0100

    refactor api call in scenariorun

--- Babylon/commands/api/scenarioruns/stop.py
+++ Babylon/commands/api/scenarioruns/stop.py
@@ -32,7 +32,8 @@ def stop(state: Any, azure_token: str, organization_id: str, scenariorun_id: str
     logger.info(f"stopping scenariorun: {state['api']['scenariorun_id']}")
     service = ScenarioRunService(state=state, azure_token=azure_token)
     response = service.stop()
+    logs = response.json()
 
     if response is None:
         return CommandResponse.fail()
-    return CommandResponse.success(response.json(), verbose=True)
+    return CommandResponse.success(logs, verbose=True)
