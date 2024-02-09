d23a2b0f673a6a43190206e1a2af9b9699830976
Elena Sasova <sassova.elena@gmail.com>
2024-02-09 16:30:03 +0100

    feat: refactor workspace commands using new api service

--- Babylon/commands/api/workspaces/update.py
+++ Babylon/commands/api/workspaces/update.py
@@ -2,20 +2,25 @@ import pathlib
 
 from logging import getLogger
 from typing import Any
-from click import Path, argument
+from click import Path
 from click import command
 from click import option
+
+from Babylon.commands.api.workspaces.service.api import WorkspaceService
 from Babylon.utils.credentials import pass_azure_token
-from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
+from Babylon.utils.decorators import (
+    wrapcontext,
+    retrieve_state,
+)
 from Babylon.utils.decorators import output_to_file
 from Babylon.utils.decorators import timing_decorator
 from Babylon.utils.environment import Environment
-from Babylon.utils.request import oauth_request
 from Babylon.utils.response import CommandResponse
-from Babylon.utils.typing import QueryType
 
 logger = getLogger("Babylon")
 env = Environment()
+
+payload = {"name": "Updated workspace to be deleted"}
 
 
 @command()
@@ -23,38 +28,44 @@ env = Environment()
 @timing_decorator
 @output_to_file
 @pass_azure_token("csm_api")
-@option("--organization-id", "org_id", type=QueryType(), help="Organization Id Cosmotech Platform")
-@option("--file",
-        "workspace_file",
-        type=Path(path_type=pathlib.Path),
-        help="Your custom workspace description file yaml")
-@argument("id", type=QueryType())
-@inject_context_with_resource({"api": ['url', 'organization_id', 'workspace_id']})
+@retrieve_state
+@option(
+    "--organization-id",
+    "organization_id",
+    type=str,
+    help="Organization Id Cosmotech Platform",
+)
+@option(
+    "--payload",
+    "payload",
+    type=Path(path_type=pathlib.Path),
+    help="Your custom workspace description file yaml",
+)
+@option("--workspace-id", "workspace_id", type=str)
 def update(
-    context: Any,
-    org_id: str,
-    id: str,
-    workspace_file: pathlib.Path,
+    state: Any,
+    organization_id: str,
+    workspace_id: str,
+    payload: pathlib.Path,
     azure_token: str,
 ) -> CommandResponse:
     """
     Update a workspace
     """
-    organization_id = org_id or context['api_organization_id']
-    workspace_id = id or context['api_workspace_id']
+    service_state = state["services"]
+    service_state["api"]["organization_id"] = (organization_id or state["services"]["api"]["organization_id"])
+    service_state["api"]["workspace_id"] = (workspace_id or state["services"]["api"]["workspace_id"])
 
-    path_file = f"{env.context_id}.{env.environ_id}.workspace.yaml"
-    workspace_file = workspace_file or env.working_dir.payload_path / path_file
-    if not workspace_file.exists():
-        logger.info("File not found")
-        return CommandResponse.fail()
-    details = env.fill_template(workspace_file)
-    response = oauth_request(f"{context['api_url']}/organizations/{organization_id}/workspaces/{workspace_id}",
-                             azure_token,
-                             type="PATCH",
-                             data=details)
+    details = env.fill_template(payload)
+
+    workspace_service = WorkspaceService(state=service_state, azure_token=azure_token, spec=details)
+    response = workspace_service.update()
     if response is None:
         return CommandResponse.fail()
     workspace = response.json()
-    logger.info(f"Successfully updated workspace {workspace['id']}")
+    if workspace_id:
+        state["services"]["api"]["workspace_id"] = workspace["id"]
+        env.store_state_in_local(state)
+        env.store_state_in_cloud(state)
+        logger.info(f"Scenario {workspace_id} has been successfully added in state")
     return CommandResponse.success(workspace, verbose=True)
