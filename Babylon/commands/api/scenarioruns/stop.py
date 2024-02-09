e00362624b716fadf53bc8adb5d5f5c2dd9862ad
Leopold Cramer <leopold.h.cramer@gmail.com>
2024-02-09 16:35:18 +0100

    changed naming of api call results

--- Babylon/commands/api/scenarioruns/stop.py
+++ Babylon/commands/api/scenarioruns/stop.py
@@ -32,8 +32,8 @@ def stop(state: Any, azure_token: str, organization_id: str, scenariorun_id: str
     logger.info(f"stopping scenariorun: {state['api']['scenariorun_id']}")
     service = ScenarioRunService(state=state, azure_token=azure_token)
     response = service.stop()
-    logs = response.json()
+    stopped_run = response.json()
 
     if response is None:
         return CommandResponse.fail()
-    return CommandResponse.success(logs, verbose=True)
+    return CommandResponse.success(stopped_run, verbose=True)
