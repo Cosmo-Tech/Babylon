110c2b6ea51d848ce9c30e7db7af0d26385c75df
Leopold Cramer <leopold.h.cramer@gmail.com>
2024-02-12 11:06:59 +0100

    refactor solution api commands

--- Babylon/commands/api/solutions/get_all.py
+++ Babylon/commands/api/solutions/get_all.py
@@ -1,19 +1,18 @@
-import jmespath
-
 from logging import getLogger
 from typing import Any, Optional
+
+import jmespath
 from click import command
 from click import option
-from Babylon.utils.decorators import inject_context_with_resource, wrapcontext
-from Babylon.utils.decorators import timing_decorator
-from Babylon.utils.response import CommandResponse
-from Babylon.utils.decorators import output_to_file
+
+from Babylon.commands.api.solutions.service.api import SolutionService
 from Babylon.utils.credentials import pass_azure_token
-from Babylon.utils.request import oauth_request
-from Babylon.utils.environment import Environment
+from Babylon.utils.decorators import output_to_file
+from Babylon.utils.decorators import timing_decorator
+from Babylon.utils.decorators import wrapcontext, retrieve_state
+from Babylon.utils.response import CommandResponse
 
 logger = getLogger("Babylon")
-env = Environment()
 
 
 @command()
@@ -21,18 +20,29 @@ env = Environment()
 @timing_decorator
 @output_to_file
 @pass_azure_token("csm_api")
+@option("--organization-id", "organization_id", type=str)
+@option("--solution-id", "solution_id", type=str)
 @option("--filter", "filter", help="Filter response with a jmespath query")
-@inject_context_with_resource({"api": ['url', 'organization_id']})
-def get_all(context: Any, azure_token: str, filter: Optional[str] = None) -> CommandResponse:
+@retrieve_state
+def get_all(state: Any, azure_token: str, organization_id: str, solution_id: str,
+            filter: Optional[str] = None) -> CommandResponse:
     """
     Get all solutions details
     """
-    logger.info(f"Getting all solutions from organization {context['api_organization_id']}")
-    response = oauth_request(f"{context['api_url']}/organizations/{context['api_organization_id']}/solutions",
-                             azure_token)
+    state = state['state']
+    state['api']['organization_id'] = organization_id or state['api']['organization_id']
+    state['api']['solution_id'] = solution_id or state['api']['solution_id']
+    if state['api']['solution_id'] is None:
+        logger.error(f"solution : {state['api']['solution_id']} does not exist")
+        return CommandResponse.fail()
+
+    logger.info(f"Getting all solutions from organization {state['api']['organization_id']}")
+    service = SolutionService(state=state, azure_token=azure_token)
+    response = service.get_all()
+    solutions = response.json()
+
     if response is None:
         return CommandResponse.fail()
-    solutions = response.json()
     if len(solutions) and filter:
         solutions = jmespath.search(filter, solutions)
     return CommandResponse.success(solutions, verbose=True)
