b7bf63d0b5457c22d48717616812d1352dae6c19
Leopold Cramer <leopold.h.cramer@gmail.com>
2024-02-13 17:19:53 +0100

    add security commands to dataset

---
+++ Babylon/commands/api/datasets/security/delete.py
@@ -0,0 +1,34 @@
+import logging
+
+from click import argument, command, option
+
+from Babylon.commands.api.datasets.security.service.api import DatasetSecurityService
+from Babylon.utils.credentials import pass_azure_token
+from Babylon.utils.decorators import output_to_file, retrieve_state, timing_decorator
+from Babylon.utils.decorators import wrapcontext
+from Babylon.utils.environment import Environment
+from Babylon.utils.response import CommandResponse
+
+logger = logging.getLogger("Babylon")
+env = Environment()
+
+
+@command()
+@wrapcontext()
+@timing_decorator
+@pass_azure_token("csm_api")
+@output_to_file
+@option("--organization-id", "organization_id", type=str)
+@option("--dataset-id", "dataset_id", type=str)
+@argument("identity_id", type=str)
+@retrieve_state
+def delete(state: dict, azure_token: str, identity_id: str, organization_id: str, dataset_id: str) -> CommandResponse:
+    """
+    Delete dataset users RBAC access
+    """
+    service_state = state["services"]
+    service_state["api"]["organization_id"] = organization_id or service_state["api"]["organization_id"]
+    service_state["api"]["dataset_id"] = dataset_id or service_state["api"]["dataset_id"]
+    service = DatasetSecurityService(azure_token=azure_token, state=service_state)
+    response = service.delete(id=identity_id)
+    return CommandResponse.success(response, verbose=True)
