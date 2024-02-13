02fbbbab7675fb733e2aba10e28b1a8fb3d10cd7
Elena Sasova <sassova.elena@gmail.com>
2024-02-13 09:10:06 +0100

    refactor: [PROF-13066] refactor dataset commands with api service

--- Babylon/commands/api/datasets/get_all.py
+++ Babylon/commands/api/datasets/get_all.py
@@ -1,8 +1,10 @@
 from logging import getLogger
 from typing import Any, Optional
+
+import jmespath
 from click import command
 from click import option
-from Babylon.commands.api.datasets.service.api import ApiDatasetService
+from Babylon.commands.api.datasets.service.api import DatasetService
 from Babylon.utils.decorators import retrieve_state, wrapcontext
 from Babylon.utils.decorators import timing_decorator
 from Babylon.utils.response import CommandResponse
@@ -19,13 +21,19 @@ env = Environment()
 @timing_decorator
 @output_to_file
 @pass_azure_token("csm_api")
+@option("--organization-id", "organization_id", type=str)
 @option("--filter", "filter", help="Filter response with a jmespath query")
 @retrieve_state
-def get_all(state: Any, azure_token: str, filter: Optional[str] = None) -> CommandResponse:
+def get_all(state: Any, azure_token: str, organization_id: str, filter: Optional[str] = None) -> CommandResponse:
     """
     Get all datasets from the organization
     """
     service_state = state["services"]
-    service = ApiDatasetService(azure_token=azure_token, state=service_state)
-    response = service.get_all(filter=filter)
-    return CommandResponse.success(response, verbose=True)
+    service_state["api"]["organization_id"] = (organization_id or service_state["api"]["organization_id"])
+    logger.info(f"Getting all datasets from organization {service_state['api']['organization_id']}")
+    service = DatasetService(azure_token=azure_token, state=service_state)
+    response = service.get_all()
+    datasets = response.json()
+    if len(datasets) and filter:
+        datasets = jmespath.search(filter, datasets)
+    return CommandResponse.success(datasets, verbose=True)
