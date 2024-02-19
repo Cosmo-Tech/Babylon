15db3c0876c034812ff31c9a4d366499a88d0b0f
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-19 16:22:17 +0100

    dix worspace sec

--- Babylon/commands/api/organizations/security/__init__.py
+++ Babylon/commands/api/organizations/security/__init__.py
@@ -4,6 +4,7 @@ from .add import add
 from .update import update
 from .delete import delete
 from .get_all import get_all
+from .set_default import set_default
 
 
 @group()
@@ -12,7 +13,7 @@ def security():
     pass
 
 
-list_commands = [add, get, update, delete, get_all]
+list_commands = [add, get, update, delete, get_all, set_default]
 
 for _command in list_commands:
     security.add_command(_command)
