7d740984f9f0a15bd7b398601b419c008a0d9632
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-19 17:51:54 +0100

    add apply svc

--- Babylon/commands/api/solutions/apply.py
+++ Babylon/commands/api/solutions/apply.py
@@ -1,19 +1,19 @@
-import json
 import sys
-import click
+import json
 import yaml
+import click
 
-from logging import getLogger
 from pathlib import Path
 from select import select
+from logging import getLogger
 from click import command, option
 from mako.template import Template
-from Babylon.commands.api.datasets.service.api import DatasetService
-from Babylon.utils.credentials import pass_azure_token
-from Babylon.utils.decorators import output_to_file, retrieve_state, injectcontext
 from Babylon.utils.environment import Environment
-from Babylon.utils.response import CommandResponse
 from Babylon.utils.yaml_utils import yaml_to_json
+from Babylon.utils.response import CommandResponse
+from Babylon.utils.credentials import pass_azure_token
+from Babylon.commands.api.solutions.service.api import SolutionService
+from Babylon.utils.decorators import output_to_file, retrieve_state, injectcontext
 
 logger = getLogger("Babylon")
 env = Environment()
@@ -24,11 +24,10 @@ env = Environment()
 @output_to_file
 @pass_azure_token("csm_api")
 @option("--organization-id", "organization_id", type=str)
-@option("--dataset-id", "dataset_id", type=str)
+@option("--solution-id", "solution_id", type=str)
 @option("--payload-file", "payload_file", type=Path)
 @retrieve_state
-def apply(state: dict, azure_token: str, organization_id: str, dataset_id: str, payload_file: Path):
-    """Apply dataset deployment"""
+def apply(state: dict, azure_token: str, organization_id: str, solution_id: str, payload_file: Path):
     service_state = state["services"]
     data = None
     if select([
@@ -54,24 +53,24 @@ def apply(state: dict, azure_token: str, organization_id: str, dataset_id: str, 
     payload_dict: dict = json.loads(payload_json)
     organization_id = payload_dict.get("organization_id") or (organization_id
                                                               or service_state["api"].get("organization_id"))
-    dataset_id = payload_dict.get("id") or (dataset_id or service_state["api"].get("dataset_id"))
+    solution_id = payload_dict.get("solution_id") or (solution_id or service_state["api"].get("solution_id"))
 
     spec = dict()
     spec["payload"] = payload_json
     service_state["api"]["organization_id"] = organization_id
-    service_state["api"]["dataset_id"] = dataset_id
-    dataset_service = DatasetService(azure_token=azure_token, spec=spec, state=service_state)
-    if not dataset_id:
-        response = dataset_service.create()
-        dataset = response.json()
-        state["services"]["api"]["dataset_id"] = dataset.get("id")
+    service_state["api"]["solution_id"] = solution_id
+    solution_service = SolutionService(azure_token=azure_token, spec=spec, state=service_state)
+    if not solution_id:
+        response = solution_service.create()
+        solution = response.json()
+        state["services"]["api"]["solution_id"] = solution.get("id")
         env.store_state_in_local(state)
         env.store_state_in_cloud(state)
     else:
-        response = dataset_service.update()
+        response = solution_service.update()
         response_json = response.json()
         old_security = response_json.get("security")
-        security_spec = dataset_service.update_security(old_security=old_security)
+        security_spec = solution_service.update_security(old_security=old_security)
         response_json["security"] = security_spec
-        dataset = response_json
-    return CommandResponse.success(dataset, verbose=True)
+        solution = response_json
+    return CommandResponse.success(solution, verbose=True)
