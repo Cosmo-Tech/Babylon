fd04719eb3e1bff4afda08ddffd65c17f4bb229a
Diamantis Sellis <diamantis.sellis@cosmotech.com>
2024-02-20 17:00:52 +0100

    feat: Basic structure of command group

--- Babylon/commands/abba/__init__.py
+++ Babylon/commands/abba/__init__.py
@@ -1,13 +1,21 @@
 from click import group
+from Babylon.commands.abba.run import run, check
+from Babylon.utils.environment import Environment
+
+env = Environment()
 
 
 @group()
 def abba():
     """Cosmotech ABBA"""
-    pass
+    env.check_environ(["BABYLON_SERVICE", "BABYLON_TOKEN", "BABYLON_ORG_NAME"])
 
 
+list_commands = [run, check]
 list_groups = []
 
 for _group in list_groups:
     abba.add_command(_group)
+
+for _command in list_commands:
+    abba.add_command(_command)
