91b0d4a3e3ed6ce24d9b78a967d11edc00428040
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-20 10:20:02 +0100

    fix typo

--- scripts/migrate_config_to_state.py
+++ scripts/migrate_config_to_state.py
@@ -7,12 +7,10 @@ import uuid
 from glob import glob
 from pathlib import Path
 
-
 parser = argparse.ArgumentParser(description="get context")
 parser.add_argument("config_path", type=str)
 parser.add_argument("context", type=str)
 parser.add_argument("platform", type=str)
-
 
 rsc = [
     "acr",
@@ -32,9 +30,7 @@ rsc = [
 def get_config_files(cnf: Path, ctx: str, plt: str):
     """Retrieve all configuration files"""
     checking = []
-    existing_files_platform = list(
-        map(lambda x: glob(str(cnf / f"*.{plt}.{x}.yaml")), rsc)
-    )
+    existing_files_platform = list(map(lambda x: glob(str(cnf / f"*.{plt}.{x}.yaml")), rsc))
     for item in existing_files_platform:
         checking += item
     if not checking:
@@ -45,7 +41,7 @@ def get_config_files(cnf: Path, ctx: str, plt: str):
     _list = list(map(lambda x: glob(str(cnf / f"{ctx}.{plt}.{x}.yaml")), rsc))
     if not _list:
         print(f"ERROR: context={ctx}, platform={plt}")
-        print(f"{ctx} context doesn't match with your configuration files")
+        print(f"{ctx} context not match with your configuration files")
         sys.exit(1)
     for item in _list:
         item = item[0] if len(_list) else ""
@@ -69,7 +65,7 @@ def migrate(ctx: str, plt: str, config_path: str) -> list[Path]:
     state["id"] = str(uuid.uuid4())
     state["context"] = ctx
     state["platform"] = plt
-    state["state"] = response
+    state["services"] = response
     state_dir = Path().home() / ".config/cosmotech/babylon"
     if not state_dir.exists():
         state_dir.mkdir(parents=True, exist_ok=True)
@@ -86,11 +82,9 @@ def checking_migration(ctx: str, plt: str) -> bool:
 def check_config_directory(config_path: str):
     config_dir = Path(config_path).absolute()
     if not config_dir.exists():
-        print(
-            f"""
+        print(f"""
 We are sorry, we can't process your request.
-Babylon configuration files {config_dir} not found"""
-        )
+Babylon configuration files {config_dir} not found""")
         print(Path().absolute())
         sys.exit(1)
 
