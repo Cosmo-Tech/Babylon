f5d8e2b6cff087f0a6962906ca52b6b096178d75
nibaldo <nibaldo.donoso@cosmotech.com>
2024-02-21 09:38:36 +0100

    fix blobclient storage name

--- Babylon/services/blob.py
+++ Babylon/services/blob.py
@@ -1,8 +1,7 @@
 from azure.storage.blob import BlobServiceClient
 
 
-def blob_client(state: dict, account_secret):
-    account_name = state["services"]["azure"]["storage_account_name"]
-    prefix = f"DefaultEndpointsProtocol=https;AccountName={account_name}"
+def blob_client(storage_name: str, account_secret):
+    prefix = f"DefaultEndpointsProtocol=https;AccountName={storage_name}"
     connection_str = (f"{prefix};AccountKey={account_secret};EndpointSuffix=core.windows.net")
     return BlobServiceClient.from_connection_string(connection_str)
