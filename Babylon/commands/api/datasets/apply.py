73c961caba603189495ef5e217b66b8d2d8cc763
Elena Sasova <sassova.elena@gmail.com>
2024-02-22 17:02:27 +0100

    refactor: [PROD-13129] refactor dataset apply command

--- Babylon/commands/api/datasets/apply.py
+++ Babylon/commands/api/datasets/apply.py
@@ -1,5 +1,8 @@
 import json
+import pathlib
 import sys
+import time
+
 import click
 import yaml
 
@@ -25,16 +28,31 @@ env = Environment()
 @output_to_file
 @pass_azure_token("csm_api")
 @option("--organization-id", "organization_id", type=str)
+@option("--workspace-id", "workspace_id", type=str)
 @option("--dataset-id", "dataset_id", type=str)
 @option("--payload-file", "payload_file", type=Path)
+@option("--dataset-zip", "dataset_zip", type=Path)
 @retrieve_state
-def apply(state: dict, azure_token: str, organization_id: str, dataset_id: str, payload_file: Path):
+def apply(
+    state: dict,
+    azure_token: str,
+    organization_id: str,
+    workspace_id: str,
+    dataset_id: str,
+    payload_file: Path,
+    dataset_zip: Path,
+):
     """Apply dataset deployment"""
     service_state = state["services"]
     data = None
-    if select([
+    if select(
+        [
             sys.stdin,
-    ], [], [], 0.0)[0]:
+        ],
+        [],
+        [],
+        0.0,
+    )[0]:
         stream = click.get_text_stream("stdin")
         data = stream.read()
     else:
@@ -53,18 +71,76 @@ def apply(state: dict, azure_token: str, organization_id: str, dataset_id: str, 
     payload = t.render(**vars, services=flattenstate)
     payload_json = yaml_to_json(payload)
     payload_dict: dict = json.loads(payload_json)
-    organization_id = payload_dict.get("organization_id") or (organization_id
-                                                              or service_state["api"].get("organization_id"))
-    dataset_id = payload_dict.get("id") or (dataset_id or service_state["api"].get("dataset_id"))
+    organization_id = payload_dict.get("organization_id") or (
+        organization_id or service_state["api"].get("organization_id")
+    )
+    service_state["api"]["workspace_id"] = (
+        workspace_id or state["services"]["api"]["workspace_id"]
+    )
+
+    dataset_id = payload_dict.get("id") or (
+        dataset_id or service_state["api"].get("dataset_id")
+    )
 
     spec = dict()
     spec["payload"] = payload_json
     service_state["api"]["organization_id"] = organization_id
     service_state["api"]["dataset_id"] = dataset_id
-    dataset_service = DatasetService(azure_token=azure_token, spec=spec, state=service_state)
+    dataset_service = DatasetService(
+        azure_token=azure_token, spec=spec, state=service_state
+    )
     if not dataset_id:
+        source_type = payload_dict["sourceType"]
+        if source_type == "File":
+            if not dataset_zip:
+                logger.error(
+                    "Zip archive is mandatory to create a dataset with sourceType File"
+                )
+                sys.exit(1)
+            elif not dataset_zip.exists():
+                logger.error(f"File {dataset_zip} not found in directory")
+                sys.exit(1)
+            elif dataset_zip.suffix != ".zip":
+                logger.error("File to create a dataset must be a zip")
+                sys.exit(1)
         response = dataset_service.create()
+        if response is None:
+            return CommandResponse.fail()
         dataset = response.json()
+        if source_type != "None":
+            if source_type in ["ADT", "AzureStorage"]:
+                dataset_service.refresh(dataset_id=dataset["id"])
+            elif source_type == "File":
+                dataset_service.upload(dataset_id=dataset["id"], zip_file=dataset_zip)
+
+            status = dataset_service.get_status(dataset_id=dataset["id"])
+            status_text = str(status.text)
+            while "PENDING" in status_text:
+                logger.info("Polling twingraph creation status...")
+                time.sleep(10)
+                status = dataset_service.get_status(dataset_id=dataset["id"])
+                status_text = str(status.text)
+            if "SUCCESS" not in status_text:
+                logger.error(
+                    f"Dataset {dataset['id']} has been created but the creation of a twingraph has failed"
+                )
+            else:
+                logger.info("Twingraph successfully created")
+        logger.info(f"Successfully created dataset {dataset['id']}")
+        if service_state["api"]["workspace_id"]:
+            linked_dataset_response = dataset_service.link_to_workspace(
+                dataset_id=dataset["id"]
+            )
+            if linked_dataset_response is None:
+                logger.error(
+                    f"Failed to link dataset {dataset['id']} to workspace {service_state['api']['workspace_id']}"
+                )
+            else:
+                logger.error(
+                    f"Dataset {dataset['id']} successfully linked "
+                    f"to workspace {service_state['api']['workspace_id']}"
+                )
+
     else:
         response = dataset_service.update()
         response_json = response.json()
