c721b16e00b1d706fbb9519cc7dfd0ef9d21ba4a
Michel Guillon <michel.guillon@cosmotech.com>
2024-02-29 16:42:13 +0100

    Fix errors

--- Babylon/commands/api/organizations/update.py
+++ Babylon/commands/api/organizations/update.py
@@ -2,7 +2,7 @@ import pathlib
 
 from logging import getLogger
 from typing import Any
-from click import Path, argument, command
+from click import Path, argument, command, option
 from Babylon.utils.decorators import retrieve_state, timing_decorator
 from Babylon.utils.decorators import injectcontext
 from Babylon.utils.response import CommandResponse
@@ -20,15 +20,17 @@ env = Environment()
 @timing_decorator
 @output_to_file
 @pass_azure_token("csm_api")
+@option("--organization-id", "organization_id", type=str)
 @argument("payload_file", type=Path(path_type=pathlib.Path, exists=True))
 @retrieve_state
-def update(state: Any, azure_token: str, payload_file: pathlib.Path) -> CommandResponse:
+def update(state: Any, azure_token: str, organization_id: str, payload_file: pathlib.Path) -> CommandResponse:
     """
     Update an organization
     """
     service_state = state["services"]
     spec = dict()
-    spec["payload"] = env.fill_template(payload_file)
+    with open(payload_file, 'r') as f:
+        spec["payload"] = env.fill_template(f.read(), state)
     organizations_service = OrganizationService(state=service_state, azure_token=azure_token, spec=spec)
     response = organizations_service.update()
     if response is None:
