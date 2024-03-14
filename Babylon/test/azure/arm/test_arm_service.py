9eff0b99403f09f5b1699ff4870afdf4f6a64b83
Michel Guillon <michel.guillon@cosmotech.com>
2024-03-14 09:37:52 +0100

    Completed tests for azure arm

--- Babylon/test/azure/arm/test_arm_service.py
+++ Babylon/test/azure/arm/test_arm_service.py
@@ -1,11 +1,15 @@
+import mock
 import os
 import unittest
 from pathlib import Path
 from click.testing import CliRunner
 from ruamel.yaml import YAML
 from Babylon.commands.azure.arm.create import create
+from Babylon.commands.azure.arm.delete import delete
+from Babylon.commands.azure.arm.get_all import get_all
 from Babylon.utils.environment import Environment
 from Babylon.utils import BABYLON_PATH
+
 
 env = Environment()
 
@@ -30,6 +34,23 @@ class AzureAppInsightServiceTestCase(unittest.TestCase):
         assert arm_deployment["template_uri"] == "https://my-template-uri"
 
         deployment_file.unlink()
+
+    @mock.patch('azure.mgmt.resource.ResourceManagementClient.deployments')
+    def test_delete(self, mock_delete):
+        result = CliRunner().invoke(
+            delete,
+            ["-D", "my-deployment"],
+            standalone_mode=False)
+
+        assert result.return_value.status_code == 0   
+
+    @mock.patch('azure.mgmt.resource.ResourceManagementClient.deployments')
+    def test_get_all(self, mock_get_all):
+        result = CliRunner().invoke(
+            get_all,
+            standalone_mode=False)
+
+        assert result.return_value.status_code == 0   
 
 
 if __name__ == "__main__":
