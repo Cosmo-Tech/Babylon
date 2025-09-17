54f06320135d1cb6c0278c67d37ff00391cf951b
Mohcine Tor <tor.mohcine@gmail.com>
2025-09-17 14:58:06 +0200

    change remote to False to run tests && fix some formating format

--- Babylon/commands/macro/deploy_connector.py
+++ Babylon/commands/macro/deploy_connector.py
@@ -29,7 +29,7 @@ def deploy_connector(namespace: str, file_content: str):
     else:
         azure_token = get_keycloak_token()
     payload: dict = content.get("spec").get("payload", {})
-    state["services"]["api"]["connector.storage_id"] = (payload.get("id") 
+    state["services"]["api"]["connector.storage_id"] = (payload.get("id")
                                                         or state["services"]["api"]["connector.storage_id"])
     spec = dict()
     spec["payload"] = json.dumps(payload, indent=2, ensure_ascii=True)
