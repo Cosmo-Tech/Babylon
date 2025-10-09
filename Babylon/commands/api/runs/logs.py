538fae598c159d91316cac65c24661d530483523
Diamantis Sellis <diamantis.sellis@cosmotech.com>
2025-10-09 11:42:33 +0200

    feat: use keycloak authorization

--- Babylon/commands/api/runs/logs.py
+++ Babylon/commands/api/runs/logs.py
@@ -3,7 +3,7 @@ from typing import Any
 
 from click import command, option
 from Babylon.commands.api.runs.services.run_api_svc import RunService
-from Babylon.utils.credentials import pass_azure_token
+from Babylon.utils.credentials import pass_keycloak_token
 from Babylon.utils.decorators import injectcontext, retrieve_state, output_to_file
 from Babylon.utils.response import CommandResponse
 
@@ -13,11 +13,11 @@ logger = getLogger("Babylon")
 @command()
 @injectcontext()
 @output_to_file
-@pass_azure_token("csm_api")
+@pass_keycloak_token()
 @option("--organization-id", "organization_id", type=str)
 @option("--run-id", "run_id", type=str)
 @retrieve_state
-def logs(state: Any, azure_token: str, organization_id: str, run_id: str) -> CommandResponse:
+def logs(state: Any, keycloak_token: str, organization_id: str, run_id: str) -> CommandResponse:
     """
     Get the logs for the Run
     """
@@ -25,7 +25,7 @@ def logs(state: Any, azure_token: str, organization_id: str, run_id: str) -> Com
     service_state['api']['organization_id'] = organization_id or service_state['api']['organization_id']
     service_state['api']['run_id'] = run_id or service_state['api'].get('run_id')
     logger.info(f"Getting logs for run: {service_state['api']['run_id']}")
-    service = RunService(state=service_state, azure_token=azure_token)
+    service = RunService(state=service_state, keycloak_token=keycloak_token)
     response = service.logs()
     if response is None:
         return CommandResponse.fail()
--- Babylon/commands/api/runs/services/run_api_svc.py
+++ Babylon/commands/api/runs/services/run_api_svc.py
@@ -8,9 +8,9 @@ logger = getLogger("Babylon")
 
 class RunService:
 
-    def __init__(self, azure_token: str, state: dict):
+    def __init__(self, keycloak_token: str, state: dict):
         self.state = state
-        self.azure_token = azure_token
+        self.keycloak_token = keycloak_token
         self.url = self.state["api"]["url"]
         self.organization_id = self.state["api"]["organization_id"]
         self.run_id = self.state["api"]["run_id"]
@@ -24,23 +24,23 @@ class RunService:
     def logs(self):
         response = oauth_request(
             f"{self.url}/organizations/{self.organization_id}/runs/{self.run_id}/logs",
-            self.azure_token)
+            self.keycloak_token)
         return response
 
     def cumulated_logs(self):
         response = oauth_request(
             f"{self.url}/organizations/{self.organization_id}/runs/{self.run_id}/cumulatedlogs",
-            self.azure_token)
+            self.keycloak_token)
         return response
 
     def status(self):
         response = oauth_request(
             f"{self.url}/organizations/{self.organization_id}/runs/{self.run_id}/status",
-            self.azure_token)
+            self.keycloak_token)
         return response
 
     def stop(self):
         response = oauth_request(
             f"{self.url}/organizations/{self.organization_id}/runs/{self.run_id}/stop",
-            self.azure_token, "POST")
+            self.keycloak_token, "POST")
         return response
