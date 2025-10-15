e5b5539d6c37957ad0aa3867ecf7d77a7e8babce
Mohcine Tor <mohcine.tor@cosmotech.com>
2025-10-15 14:52:36 +0200

    feat: add get_contexts command to display current contexts

---
+++ Babylon/commands/namespace/get_contexts.py
@@ -0,0 +1,22 @@
+from logging import getLogger
+from click import command
+from Babylon.utils.response import CommandResponse
+from Babylon.utils.environment import Environment
+
+logger = getLogger("Babylon")
+env = Environment()
+
+
+@command()
+def get_contexts() -> CommandResponse:
+    """Display the currently active namespace"""
+    namespace = env.get_namespace_from_local()
+    headers = ["CURRENT", "CONTEXT", "TENANT", "STATE ID"]
+    values = ["*", namespace.get("context", ""), namespace.get("platform", ""), namespace.get("state_id", "")]
+
+    col_widths = [max(len(h), len(v)) + 2 for h, v in zip(headers, values)]
+    header_line = "".join(h.ljust(w) for h, w in zip(headers, col_widths))
+    value_line = "".join(v.ljust(w) for v, w in zip(values, col_widths))
+    logger.info(header_line)
+    logger.info(value_line)
+    return CommandResponse.success()
