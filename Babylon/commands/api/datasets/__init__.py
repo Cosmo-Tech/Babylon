87261dbbcedab7abdd7e23e06168fc1f4266863a
Diamantis Sellis <diamantis.sellis@cosmotech.com>
2025-10-15 09:44:00 +0200

    refactor: remove unsuported endpoints
    Remove the code that does not work, these should be
    reimplemented in the future.

--- Babylon/commands/api/datasets/__init__.py
+++ Babylon/commands/api/datasets/__init__.py
@@ -1,13 +1,11 @@
 from click import group
 
 from .apply import apply
-from .create import create
 from .delete import delete
 from .get import get
 from .get_all import get_all
 from .search import search
 from .security import security
-from .update import update
 
 
 @group()
@@ -16,7 +14,7 @@ def datasets():
     pass
 
 
-list_commands = [delete, update, get, create, search, get_all, apply]
+list_commands = [delete, get, search, get_all, apply]
 
 for _command in list_commands:
     datasets.add_command(_command)
--- Babylon/test/api/datasets/test_datasets_service.py
+++ Babylon/test/api/datasets/test_datasets_service.py
@@ -2,12 +2,10 @@ import unittest
 from unittest import mock
 from click.testing import CliRunner
 from Babylon.commands.api.datasets.services.datasets_api_svc import DatasetService
-from Babylon.commands.api.datasets.create import create
 from Babylon.commands.api.datasets.get import get
 from Babylon.commands.api.datasets.get_all import get_all
 from Babylon.commands.api.datasets.delete import delete
 from Babylon.commands.api.datasets.search import search
-from Babylon.commands.api.datasets.update import update
 from Babylon.utils.environment import Environment
 from requests.models import Response
 
@@ -21,32 +19,6 @@ class DatasetServiceTestCase(unittest.TestCase):
         env.check_environ(["BABYLON_SERVICE", "BABYLON_TOKEN", "BABYLON_ORG_NAME"])
         env.get_namespace_from_local()
         env.remote = False
-
-    @mock.patch.object(DatasetService, 'create')
-    def test_create(self, mock_create):
-        create_response = Response()
-        create_response.status_code = 201
-        create_response._content = b'{"id": "1", "name": "Dataset Name", "sourceType": "None"}'
-        mock_create.return_value = create_response
-
-        payload = str(env.pwd / "Babylon/test/api/datasets/payload.json")
-        file_zip = str(env.pwd / "Babylon/test/api/datasets/test.zip")
-        result = CliRunner().invoke(
-            create,
-            [
-                "--organization-id",
-                "my_organization_id",
-                "--workspace-id",
-                "my_workspace_id",
-                "--dataset-zip",
-                file_zip,
-                payload,
-            ],
-            standalone_mode=False,
-        )
-
-        assert result.return_value.data == {"id": "1", "name": "Dataset Name", "sourceType": "None"}
-        assert result.exit_code == 0
 
     @mock.patch.object(DatasetService, "get")
     def test_get(self, datasetservice_get):
@@ -98,27 +70,6 @@ class DatasetServiceTestCase(unittest.TestCase):
         result = CliRunner().invoke(
             search,
             ["--organization-id", "my_organization_id", "atag"],
-            standalone_mode=False,
-        )
-
-        assert result.return_value.data == {"id": "1", "name": "ADT Dataset"}
-
-    @mock.patch.object(DatasetService, "update")
-    def test_update(self, datasetservice_update):
-        the_response = Response()
-        the_response.status_code = 200
-        the_response._content = b'{"id": "1", "name": "ADT Dataset"}'
-        datasetservice_update.return_value = the_response
-        payload_file = str(env.pwd / "Babylon/test/api/datasets/payload.json")
-        result = CliRunner().invoke(
-            update,
-            [
-                "--organization-id",
-                "my_organization_id",
-                "--dataset-id",
-                "1",
-                payload_file,
-            ],
             standalone_mode=False,
         )
 
