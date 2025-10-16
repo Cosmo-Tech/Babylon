7871d20ae19d0bf124f477031a738aa3662fe47c
Mohcine Tor <mohcine.tor@cosmotech.com>
2025-10-16 10:10:12 +0200

    refactor: meta about command

--- Babylon/commands/api/meta/about.py
+++ Babylon/commands/api/meta/about.py
@@ -1,4 +1,5 @@
-from click import command
+import json
+from click import command, echo, style
 from logging import getLogger
 from Babylon.utils.request import oauth_request
 from Babylon.utils.decorators import injectcontext, output_to_file, retrieve_state
@@ -19,12 +20,17 @@ def about(state: dict, keycloak_token: str) -> dict:
     """
     Get the version of the API service
     """
+    _meta = [""]
+    _meta.append("Get the version of the API service")
+    _meta.append("")
+    echo(style("\n".join(_meta), bold=True, fg="green"))
     url = state.get("services").get("api").get("url")
     if not url:
-        logger.error("API url not found verify the state")
+        logger.error("[babylon] Api url not found verify the state")
         return CommandResponse.fail()
     response = oauth_request(f"{url}/about", keycloak_token, type="GET")
     if response is None:
         return CommandResponse.fail()
     info = response.json()
-    return CommandResponse.success(info, verbose=True)
+    logger.info(json.dumps(info, indent=2))
+    return CommandResponse.success(info)
