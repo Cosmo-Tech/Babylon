9a864e2fee1c72f2379c69822c7b3799ff55e4f3
Diamantis Sellis <diamantis.sellis@cosmotech.com>
2025-12-16 10:41:57 +0100

    feat: solution deployment with python client

--- Babylon/commands/macro/deploy_solution.py
+++ Babylon/commands/macro/deploy_solution.py
@@ -1,11 +1,13 @@
-import os
-import sys
 from json import dumps
 from logging import getLogger
 
 from click import echo, style
+from cosmotech_api.models.solution_create_request import SolutionCreateRequest
+from cosmotech_api.models.solution_security import SolutionSecurity
+from cosmotech_api.models.solution_update_request import SolutionUpdateRequest
 
-from Babylon.commands.api.solutions.services.solutions_api_svc import SolutionService
+from Babylon.commands.api.client import get_solution_api_instance
+from Babylon.commands.macro.deploy import update_object_security
 from Babylon.utils.credentials import get_keycloak_token
 from Babylon.utils.environment import Environment
 from Babylon.utils.response import CommandResponse
@@ -21,51 +23,48 @@ def deploy_solution(namespace: str, file_content: str) -> bool:
     echo(style("\n".join(_ret), bold=True, fg="green"))
     env.get_ns_from_text(content=namespace)
     state = env.retrieve_state_func()
-    vars = env.get_variables()
-    metadata = env.get_metadata(vars=vars, content=file_content, state=state)
+
     keycloak_token, config = get_keycloak_token()
     content = env.fill_template(data=file_content, state=state)
     payload: dict = content.get("spec").get("payload")
     api_section = state["services"]["api"]
     api_section["solution_id"] = payload.get("id") or api_section.get("solution_id", "")
-    if metadata.get("selector", ""):
-        api_section["organization_id"] = metadata["selector"].get("organization_id", "")
-    else:
-        if not api_section["organization_id"]:
-            logger.error(f"Missing 'organization_id' in metadata -> selector field : {metadata.get('selector')}")
-            sys.exit(1)
-    spec = dict()
+
+    spec = {}
     spec["payload"] = dumps(payload, indent=2, ensure_ascii=True)
-    solution_svc = SolutionService(keycloak_token=keycloak_token, spec=spec, config=config, state=api_section)
-    sidecars = content.get("spec").get("sidecars", {})
+    api_instance = get_solution_api_instance(config=config, keycloak_token=keycloak_token)
     if not api_section["solution_id"]:
         logger.info("Creating solution")
-        response = solution_svc.create()
-        if response is None:
+        solution_create_request = SolutionCreateRequest.from_dict(payload)
+        solution = api_instance.create_solution(
+            organization_id=api_section["organization_id"], solution_create_request=solution_create_request
+        )
+        if solution is None:
             return CommandResponse.fail()
-        solution = response.json()
-        logger.info(f"Solution {[solution['id']]} successfully created")
-        state["services"]["api"]["solution_id"] = solution.get("id")
+        logger.info(f"Solution {[solution.id]} successfully created")
+        state["services"]["api"]["solution_id"] = solution.id
     else:
         logger.info(f"Updating solution {[api_section['solution_id']]}")
-        response = solution_svc.update()
-        if response is None:
+        solution_update_request = SolutionUpdateRequest.from_dict(payload)
+        updated = api_instance.update_solution(
+            solution_id=api_section["solution_id"], solution_update_request=solution_update_request
+        )
+
+        if updated is None:
             return CommandResponse.fail()
-        response_json = response.json()
-        old_security = response_json.get("security")
-        security_spec = solution_svc.update_security(old_security=old_security)
-        response_json["security"] = security_spec
-        solution = response_json
-        logger.info(f"Solution {[solution['id']]} successfully updated")
+        try:
+            current_security = api_instance.get_solution_security(solution_id=api_section["solution_id"])
+            update_object_security(
+                "solution",
+                current_security=current_security,
+                desired_security=SolutionSecurity.from_dict(payload.get("security")),
+                api_instance=api_instance,
+                object_id=api_section["solution_id"],
+            )
+        except Exception as e:
+            logger.error(f"Failed to update solution security: {e}")
+            return CommandResponse.fail()
+        logger.info(f"Solution {[api_section['solution_id']]} successfully updated")
     env.store_state_in_local(state)
     if env.remote:
         env.store_state_in_cloud(state)
-    # update run_templates
-    if sidecars:
-        run_scripts = sidecars.get("run_scripts")
-        if run_scripts:
-            data = run_scripts.get("post_deploy.sh", "")
-            if data:
-                os.system(data)
-        if not solution.get("id"):
-            sys.exit(1)
