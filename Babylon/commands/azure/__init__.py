b3e573437e5ed5825cda6b64e4fbbae909589ef9
Diamantis Sellis <diamantis.sellis@cosmotech.com>
2026-01-12 13:57:15 +0100

    refactor: remove dead code

--- Babylon/commands/azure/__init__.py
+++ Babylon/commands/azure/__init__.py
@@ -17,8 +17,7 @@ list_groups = [storage, permission, token]
 @group()
 def azure():
     """Group allowing communication with Microsoft Azure Cloud"""
-    env.check_environ(["BABYLON_SERVICE", "BABYLON_TOKEN", "BABYLON_ORG_NAME"])
-
+    pass
 
 for _group in list_groups:
     azure.add_command(_group)
