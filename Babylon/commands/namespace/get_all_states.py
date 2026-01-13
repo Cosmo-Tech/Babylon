765bfd077d8bf8aedb46e929d65e34a61b1124d9
Diamantis Sellis <diamantis.sellis@cosmotech.com>
2026-01-13 12:04:37 +0100

    refactor: merge configuration path definitions

--- Babylon/commands/namespace/get_all_states.py
+++ Babylon/commands/namespace/get_all_states.py
@@ -5,14 +5,17 @@ from click import command
 from Babylon.utils.environment import Environment
 from Babylon.utils.response import CommandResponse
 
-logger = getLogger("Babylon")
+logger = getLogger(__name__)
 env = Environment()
 
 
 @command()
 def get_states() -> CommandResponse:
     """Display all states in your local machine"""
-    states_dir = env.get_all_states_from_local()
+    states_dir = env.state_dir
+    if not env.state_dir.exists():
+        logger.error(f"directory {env.state_dir} not found")
+        return CommandResponse.fail()
     states_files = sorted(states_dir.glob("state.*.yaml"))
     for f in states_files:
         print(f" {f.name}")
